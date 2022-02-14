27a11bbda4a7ddeb1c9cb3392f847f47958d89c2
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-02-14 21:23:37 +0000

    Project 1 Done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,38 @@
-function setAlarm() {}
+function setAlarm() {
+  
+let timeOfAlarm = document.getElementById("alarmSet");
+let timeRemaining = document.getElementById("timeRemaining");
+let btn = document.getElementById("set");
+  
+
+btn.addEventListener("click",  () => {
+  let alarmTime = timeOfAlarm.value;
+  let time = alarmTime *60;
+  let timer = setInterval(updateH1,1000);
+
+  function updateH1(){
+    
+    if (time <= 0 ){ 
+      timeRemaining.innerHTML = `Time Remaining: 00:00`;
+      playAlarm();
+      
+    } else {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    timeRemaining.innerHTML = 
+    `Time Remaining: ${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+    time--;
+    }
+  }
+  
+})
+  
+ let stopButton = document.getElementById("stop");
+ stopButton.addEventListener("click", () => {
+  timeRemaining.innerHTML = `Time Remaining: 00:00`;
+});  
+
+}
 
 // DO NOT EDIT BELOW HERE
 
