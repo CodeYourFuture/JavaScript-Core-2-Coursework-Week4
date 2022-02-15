3cd365ab7afa8d89db3ed62dae15ab0c7b607302
Monika39-maker <dangolmonika@yahoo.com>
2022-02-15 14:46:57 +0000

    finished alarmClock with extra task of adding functional pause btn

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,73 @@
-function setAlarm() {}
+const allButtonsEl = document.querySelector('.centre')
+const newPauseBtn = document.createElement('button');
+newPauseBtn.innerText = 'Pause';
+allButtonsEl.appendChild(newPauseBtn)
+let intervalId;
+function setAlarm() {
+  
+  const timeRemainingEl = document.getElementById('timeRemaining');
+  const setTimerBtn = document.getElementById('set')
+  let inputTime = 0;
+  
+  
+  setTimerBtn.addEventListener('click', () => {
+    let alarmSetInput = document.getElementById('alarmSet');
+    inputTime = alarmSetInput.value;
+    intervalId = setInterval(() => {
+      controlTimer()
+    }, 1000)
+    
+    
+  });
+  newPauseBtn.addEventListener('click', () => {
+    clearInterval(intervalId)
+  })
+  function controlTimer() {
+    timeRemainingEl.innerText = 'Time Remaining: 00:' + inputTime;
+    // console.log(time)
+    inputTime -= 1;
+    
+    if (inputTime < 0) {
+      clearInterval(intervalId)
+      playAlarm();
+      
+    };
+    inputTime = '';
+    
+  }
+  
+  
+  
+};
+
+
+setAlarm();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
