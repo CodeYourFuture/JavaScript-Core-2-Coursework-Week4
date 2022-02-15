9d529fdd67469de749cb8141db3441751836ba04
Helen <g.helen.rog@gmail.com>
2022-02-15 13:12:46 +0000

    Mandatory: slideshow
    added images
    html/css/js

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,19 @@
-// Write your code here
+const slider_img = document.querySelector('.slider-img');
+const images = ['crocus.jpg', 'wheat.jpg', 'leaves.jpg', 'hemlock.jpg'];
+let i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+ images[i]);	
+}
