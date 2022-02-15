7336670b07fcd794785891fc0b618b54d4f88ec8
Margarita Glushkova <m.glushkova1@gmail.com>
2022-02-15 15:35:13 +0000

    Alarm clock with no extra

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+const setAlarm = () => {
+  let timeRemainingEl = document.getElementById("timeRemaining");
+  let inputTime = document.getElementById("alarmSet").value;
+  const outputFormat = (totalSeconds) => {
+    let m = Math.floor(totalSeconds / 60);
+    let s = totalSeconds % 60;
+    s = s < 10 ? "0" + s : s;
+    m = m < 10 ? "0" + m : m;
+    timeRemainingEl.innerText = `Time Remaining: ${m} : ${s}`;
+  };
+  outputFormat(inputTime);
+  document.getElementById("alarmSet").value = null;
+  const counter = () => {
+    inputTime--;
+    if (inputTime <= 0) {
+      playAlarm();
+      clearInterval(countDown);
+    }
+    outputFormat(inputTime);
+  };
+  let countDown = setInterval(counter, 1000);
+};
 
 // DO NOT EDIT BELOW HERE
 
