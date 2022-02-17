9eb482d542afbcd1aa32356e38e6edd4c0ac53bd
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-02-17 20:17:14 +0000

    Add alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,24 @@
-function setAlarm() {}
+const setAlarm = () => {
+  const inputEl = document.getElementById("alarmSet");
+  let userInputValue = inputEl.value;
+  const h1TimeRemainingEl = document.getElementById("timeRemaining");
+
+  let timer = setInterval(() => {
+    let minutes = Math.floor(userInputValue / 60);
+    let seconds = Math.floor(userInputValue % 60);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    h1TimeRemainingEl.innerText = `Time Remaining: ${minutes} : ${seconds}`;
+
+    userInputValue-- || clearInterval(timer);
+
+    if (userInputValue < 0) playAlarm();
+  }, 1000);
+
+  inputEl.value = "";
+};
 
 // DO NOT EDIT BELOW HERE
 
