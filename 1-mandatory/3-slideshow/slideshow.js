4d9b3959f4e86d7ee5accfc3d8c1db398e9bc4d3
Monika39-maker <dangolmonika@yahoo.com>
2022-02-17 01:09:34 +0000

    finished the mandatory imge slider project

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,71 @@
 // Write your code here
+const images = ["url('./assets/img1.jpg')", "url('./assets/img2.jpg')", "url('./assets/img3.jpg')", "url('./assets/img4.jpg')", "url('./assets/img5.jpg')"]
+
+const body = document.querySelector('body');
+const mainDiv = document.querySelector('.main-div');
+const autoBackBtn = document.getElementById("auto-back-button");
+const backBtn = document.getElementById("back-button");
+const stopBtn = document.getElementById("stop-button");
+const autoForwardBtn = document.getElementById("auto-forward-button");
+const forwardBtn = document.getElementById("forward-button")
+
+let imageSerialNumber = 0;
+let intervalId;
+
+
+mainDiv.style.backgroundImage = images[imageSerialNumber]
+
+function moveleft() {
+    imageSerialNumber++;
+    mainDiv.style.backgroundImage = images[imageSerialNumber];
+
+    if (imageSerialNumber === 5) {
+        imageSerialNumber = 0;
+        mainDiv.style.backgroundImage = images[imageSerialNumber];
+
+    }
+
+}
+
+function moveRight() {
+    imageSerialNumber--;
+    mainDiv.style.backgroundImage = images[imageSerialNumber];
+
+    if (imageSerialNumber < 0) {
+        imageSerialNumber = 5;
+        mainDiv.style.backgroundImage = images[imageSerialNumber];
+
+    }
+
+}
+
+forwardBtn.addEventListener('click', () => {
+    moveleft() 
+});
+
+backBtn.addEventListener('click', () => {
+    moveRight() 
+});
+
+autoForwardBtn.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+        moveleft()   
+    }, 3000);
+});
+
+
+autoBackBtn.addEventListener('click', () => {
+    console.log('hi')
+    intervalId = setInterval(() => {
+        moveRight()   
+    }, 3000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId)
+});
+
+
+
+
+
