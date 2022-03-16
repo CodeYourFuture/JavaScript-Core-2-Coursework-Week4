3b29eb36e34a10fafd59514a06749aeb32b68f8c
zamanisima <zamanisima.uk@gmail.com>
2022-03-16 16:49:02 +0000

    alarm clcok completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,6 +1,41 @@
-function setAlarm() {}
+let minutes = "00";
+let seconds = "00";
+let remainingTimeTag = document.getElementById("timeRemaining");
 
-// DO NOT EDIT BELOW HERE
+function setAlarm() {
+  let inputEl = document.getElementById("alarmSet");
+  let myValue = Number(inputEl.value);
+  if (myValue >= 3600) {
+    return alert("Put a number less than 3600!");
+  }
+  // else
+  let loop = () => {
+    if (myValue === -1) {
+      //Time out
+      return playAlarm();
+    }
+    minutes = myValue >= 60 ? Math.floor(myValue / 60) : "00";
+    seconds = myValue >= 60 ? myValue % 60 : myValue;
+    myValue -= 1;
+    if (String(seconds).length < 2) {
+      seconds = "0" + seconds;
+    }
+    if (String(minutes).length < 2) {
+      minutes = "0" + minutes;
+    }
+    remainingTimeTag.innerHTML = `Time Remaining: ${minutes} : ${seconds} `;
+  };
+  inputEl.value = "";
+  console.log(inputEl);
+  let repeating = setInterval(loop, 1000);
+  let btnStop = document.getElementById("stop");
+  btnStop.addEventListener("click", () => {
+    pauseAlarm();
+    clearInterval(repeating);
+  });
+}
+
+//DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
