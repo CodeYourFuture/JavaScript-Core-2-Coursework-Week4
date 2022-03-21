79cd24d7cad1f370107155462e3038bf1fb70cda
Sobia Waqar <sobiawaqar27'gmail.com>
2022-03-21 14:42:24 +0000

    completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,30 @@
 function setAlarm() {
-const timeRemaining = document.getElementById("timeRemaining")
-const setAlarmInput = document.getElementById("alarmSet")
-let takingInput = Number(setAlarmInput.value)
-timeRemaining.innerText = "Time Remaining: " + takingInput + " Sec"
+  const timeRemaining = document.getElementById("timeRemaining");
+  const setAlarmInput = document.getElementById("alarmSet");
+  const stopCounter = document.getElementById("stopCounter");
+  let takingInput = Number(setAlarmInput.value);
+  timeRemaining.innerText = "Time Remaining: " + takingInput + " Sec";
 
-const timerId = setInterval(()=> { 
-  takingInput -= 1
-  timeRemaining.innerText = "Time Remaining: " + takingInput + " Sec"
-  if (takingInput === 0){
-    clearInterval(timerId)
-    playAlarm();
-  }
-}, 1000)
+  const timerId = setInterval(() => {
+    takingInput -= 1;
+    timeRemaining.innerText = "Time Remaining: " + takingInput + " Sec";
+    if (takingInput === 0) {
+      clearInterval(timerId);
+      playAlarm();
+    }
+  }, 1000);
 
-//console.log(typeof takingInput)
-//console.log(takingInput);
+  const timerId = setInterval(() => {
+    takingInput -= 1;
+    timeRemaining.innerText = "Time Remaining: " + takingInput + " Sec";
+    if (takingInput === 0) {
+      clearInterval(timerId);
+      playAlarm();
+    }
+  }, 1000);
 }
 
-
 var audio = new Audio("alarmsound.mp3");
-
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
@@ -38,24 +43,4 @@ function pauseAlarm() {
   audio.pause();
 }
 
-window.onload = setup; // callling setup function
-
-/*
-function setAlarm() {
-  const inputField = document.getElementById("alarmSet");
-  let timer = inputField.value;
-
-  const heading = document.getElementById("timeRemaining");
-  heading.innerText = `Time Remaining: 00:${timer}`;
-
-  const myInterval = setInterval(() => {
-    timer = timer - 1;
-    heading.innerText = `Time Remaining: 00:${timer}`;
-
-    if (timer === 0) {
-      clearInterval(myInterval);
-      playAlarm();
-    }
-  }, 1000);
-}
-*/
+window.onload = setup;
