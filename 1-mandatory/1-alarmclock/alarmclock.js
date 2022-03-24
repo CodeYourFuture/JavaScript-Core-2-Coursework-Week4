d65398f43ff19397dc1c243b964890d45ff562f3
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-03-24 15:48:41 +0000

    Completed set alarm using solutions - I don't think I could have done it otherwise tbh

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,75 @@
-function setAlarm() {}
+let currentTime = 0;
+let intervalId = null;
+let warning = null;
+
+function updateTime(callback) {
+  const ONE_SECOND_IN_MILLISECONDS = 1000;
+  const timeInterval = 1;
+
+  const id = setInterval(() => {
+    const newTime = currentTime - timeInterval;
+    currentTime = newTime;
+    callback(newTime);
+  }, ONE_SECOND_IN_MILLISECONDS);
+  intervalId = id;
+}
+
+function formatTime(timeInSeconds) {
+  const minutes = `${Math.floor(timeInSeconds / 60)}`.padStart(2, '0');
+  const seconds = `${timeInSeconds % 60}`.padStart(2, '0');
+  return `${minutes}:${seconds}`;
+}
+
+function updateTimeRemainingDisplay(time) {
+   const timeRemainingDisplay = document.querySelector('#timeRemaining');
+  timeRemainingDisplay.innerText = `Time Remaining: ${formatTime(time)}`;
+}
+
+function updatePageAfterTimeUpdate(newTime) {
+  updateTimeRemainingDisplay(newTime);
+  if (newTime === 0) {
+    clearInterval(intervalId);
+    playAlarm();
+  }
+}
+
+
+function setAlarmClock() {
+  updateTime(updatePageAfterTimeUpdate);
+}
+
+function addWarning(warningMessage) {
+  const centre = document.querySelector('.centre');
+  const warningElement = document.createElement('p');
+  warningElement.innerText = warningMessage;
+  centre.appendChild(warningElement);
+  return warningElement;
+}
+
+function removeWarning() {
+  const centre = document.querySelector('.centre');
+  if (warning) centre.removeChild(warning);
+  warning = null;
+}
+
+function setAlarm() {
+  const alarmSet = document.querySelector('#alarmSet');
+
+  const alarmTimeInSeconds = parseInt(alarmSet.value);
+
+  removeWarning(warning);
+  if (!Number.isInteger(alarmTimeInSeconds)) {
+    warning = addWarning('Please enter a number!');
+    return;
+  } else if (alarmTimeInSeconds <= 0) {
+    warning = addWarning('Please enter a number bigger than 0');
+    return;
+  }
+
+  currentTime = alarmTimeInSeconds;
+  updateTimeRemainingDisplay(currentTime);
+  setAlarmClock();
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -12,6 +83,15 @@ function setup() {
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
   });
+
+  document.getElementById('play').addEventListener('click', () => {
+    if (intervalId) return;
+    else {
+      if (currentTime === 0) return;
+      startAlarmClock();
+    }
+  });
+
 }
 
 function playAlarm() {
@@ -19,6 +99,8 @@ function playAlarm() {
 }
 
 function pauseAlarm() {
+  clearInterval(intervalId);
+  intervalId = null;
   audio.pause();
 }
 
