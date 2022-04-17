c79d0ab74805739e0bce43364d30b7ce83d596b3
Jacint M. David <jacint.m.david@gmail.com>
2022-04-17 15:12:57 +0100

    Auto-Play functionality implemented

--- 1-mandatory/2-quotegenerator/quotegenerator.js
+++ 1-mandatory/2-quotegenerator/quotegenerator.js
@@ -14,11 +14,14 @@ const mainDiv = document.createElement("div"),
   toggleContainer = document.createElement("div"),
   toggle = document.createElement("div"),
   toggleButton = document.createElement("div"),
-  toggleText = document.createElement("div");
+  toggleText = document.createElement("div"),
+  autoText = document.createElement("div");
 
 newQuoteButton.innerText = "New Quote";
+autoText.innerText = "Auto-Play";
+
 toggle.append(toggleButton);
-toggleContainer.append(toggle, toggleText);
+toggleContainer.append(autoText, toggle, toggleText);
 quote.append(quoteIconLeft, quoteText, quoteIconRight);
 mainDiv.append(quote, author, newQuoteButton, toggleContainer);
 document.body.append(mainDiv);
@@ -31,7 +34,7 @@ mainDiv.style.backgroundColor = "white";
 mainDiv.style.minHeight = "250px";
 mainDiv.style.width = "550px";
 mainDiv.style.marginTop = "50px";
-mainDiv.style.padding = "40px 20px";
+mainDiv.style.padding = "40px 20px 10px";
 mainDiv.style.display = "flex";
 mainDiv.style.flexDirection = "column";
 mainDiv.style.justifyContent = "space-around";
@@ -49,14 +52,15 @@ author.style.fontSize = "1rem";
 author.style.fontStyle = "italic";
 author.style.color = "teal";
 author.style.textAlign = "right";
-author.style.margin = "30px 0";
+author.style.margin = "30px 0 60px";
 
 newQuoteButton.style.alignSelf = "flex-start";
-newQuoteButton.style.backgroundColor = "teal";
 newQuoteButton.style.color = "white";
 newQuoteButton.style.border = 0;
 newQuoteButton.style.padding = "5px 10px";
 newQuoteButton.style.borderRadius = "5px";
+newQuoteButton.setAttribute("id", "new-quote-button");
+newQuoteButton.classList.add("new-quote-button");
 
 // EXTRA TOGGLE BUTTON
 
@@ -65,6 +69,7 @@ toggle.classList.add("toggle");
 toggleButton.classList.add("toggle-button");
 toggleText.classList.add("text");
 toggleText.innerText = "OFF";
+autoText.classList.add("auto-text");
 
 // RANDOM QUOTE
 
@@ -79,14 +84,16 @@ renderNewQuote();
 newQuoteButton.addEventListener("click", renderNewQuote);
 
 // EXTRA TOGGLE FUNCTIONALITY
-
+let autoPlay;
 const animate = () => {
   toggle.classList.toggle("active");
   toggleButton.classList.toggle("active");
   if (toggle.classList.contains("active")) {
     toggleText.innerText = "ON";
+    autoPlay = setInterval(renderNewQuote, 2000);
   } else {
     toggleText.innerText = "OFF";
+    clearInterval(autoPlay);
   }
 };
 
