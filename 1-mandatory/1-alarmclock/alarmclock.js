8493cbddca74a0c63dbf1b6b28b3a030ac8b8c04
chezlinbenson <chezlinbenson11@gmail.com>
2022-08-30 21:08:28 +0200

    Complete the Mandatory alarmclock function with the extra feautures remaining

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,11 +1,44 @@
 function setAlarm() {
-  let timeRemainder = document.getElementById("timeRemaining");
+  let timeDisplay = document.getElementById("timeRemaining");
   let alarmCounter = document.getElementById("alarmSet").value;
-  let timerCount = document.getElementById("timer");
 
-  timerCount.innerText = alarmCounter;
+  let mm = "00";
+  let ss = "00";
 
-  
+  const interval = setInterval(() => {
+    mm = Math.floor(alarmCounter / 60);
+    ss = Math.floor(alarmCounter % 60);
+
+    if (alarmCounter >= 600) {
+      ss = ss < 10 ? "0" + ss : ss;
+      timeDisplay.innerText = `Time Remaining: ${mm}:${ss}`;
+      mm--;
+      ss--;
+      alarmCounter--;
+    } else if (alarmCounter < 600 && alarmCounter >= 60) {
+      ss = ss < 10 ? "0" + ss : ss;
+      mm = mm < 10 ? "0" + mm : mm;
+      timeDisplay.innerText = `Time Remaining: ${mm}:${ss}`;
+      mm--;
+      ss--;
+      alarmCounter--;
+    } else if (alarmCounter < 60 && alarmCounter >= 10) {
+      ss = ss < 10 ? "0" + ss : ss;
+      timeDisplay.innerText = `Time Remaining: 00:${ss}`;
+      ss--;
+      alarmCounter--;
+    } else {
+      ss = ss < 10 ? "0" + ss : ss;
+      timeDisplay.innerText = `Time Remaining: 00:${ss}`;
+      ss--;
+      alarmCounter--;
+    }
+
+    if (alarmCounter < 0) {
+      clearInterval(interval);
+      playAlarm();
+    }
+  }, 1000);
 }
 
 // DO NOT EDIT BELOW HERE
