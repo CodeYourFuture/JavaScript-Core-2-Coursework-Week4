53404774097d96a013bae0454b965b66cb3b66b1
chandredewet <chandredewet@gmail.com>
2022-08-30 16:31:01 +0200

    Added Set Interval, and it counted but web wont display

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+function setAlarm() {
+
+  let timeRemainEl = document.getElementById("timeRemaining");
+  let newDetails = timeRemainEl.innerText.replace(":00", ":");
+  timeRemainEl.innerText = newDetails;
+  let enteredSpanEl = document.createElement("span");
+  timeRemainEl.appendChild(enteredSpanEl);
+
+  let timeSetInputEl = document.getElementById("alarmSet");
+  let timeSetCounter = timeSetInputEl.value;
+
+  const interval = setInterval(() => {
+    timeSetCounter =
+    timeSetCounter < 10 ? "0" + timeSetCounter : timeSetCounter;
+    enteredSpanEl.textContent = timeSetCounter;
+    timeSetCounter--;
+    if (timeSetCounter< 0 ) {
+      clearInterval(interval);
+      console.log('Ding!');
+    }
+  }, 1000);
+
+  // const d = new Date();
+  // //d.setSeconds(timeSetVal);
+  // enteredSpanEl.textContent = d.setSeconds(timeSetVal);
+  /* <h1 id="timeRemaining">Time Remaining: 00:00</h1>; */
+}
 
 // DO NOT EDIT BELOW HERE
 
