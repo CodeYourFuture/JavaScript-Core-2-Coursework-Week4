1d8bfe7aa01dadc22617754eae7b7f899cf3bc51
chandredewet <chandredewet@gmail.com>
2022-09-05 15:27:55 +0200

    completed level 1, forward and back work completely

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -2,21 +2,33 @@
 //level1:
 
 pictures = [
+  "https://images.unsplash.com/photo-1660563828193-c4ea4b73dd08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   "https://images.unsplash.com/photo-1583855282680-6dbdc69b0932?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
   "https://images.unsplash.com/photo-1655844479061-21e164057c27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
   "https://images.unsplash.com/photo-1618331835717-801e976710b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   "https://images.unsplash.com/photo-1514241516423-6c0a5e031aa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-  "https://images.unsplash.com/photo-1660563828193-c4ea4b73dd08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
 ];
 
 let counter = 0;
+
 document.getElementById("forwardBtn").addEventListener("click", () => {
-  if (counter < pictures.length) {
-    document.getElementById("imageShow").src = pictures[counter];
+  if (counter < pictures.length - 1) {
     counter++;
+    document.getElementById("imageShow").src = pictures[counter];
+    console.log(counter);
   } else {
     counter = 0;
     document.getElementById("imageShow").src = pictures[counter];
-    counter++;
+    console.log(counter);
+  }
+});
+
+document.getElementById("backwardBtn").addEventListener("click", () => {
+  if (counter > 0) {
+    counter--;
+    document.getElementById("imageShow").src = pictures[counter];
+  } else {
+    counter = pictures.length - 1;
+    document.getElementById("imageShow").src = pictures[counter];
   }
 });
