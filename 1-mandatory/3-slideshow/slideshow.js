f985934dd7cd0a6bde75603b6dd8c8df2a48e7b4
chezlinbenson <chezlinbenson11@gmail.com>
2022-09-05 13:05:25 +0200

    Complete the Mandatory Slideshow Project. HTML elements, CSS styling to elements and basic Javascript declaring variables and assign id's to them as well as declare array of image links.

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,21 @@
 // Write your code here
+
+let imageCarouselArr = [
+  "https://unsplash.com/photos/6G98hiCJETA",
+  "https://unsplash.com/photos/MSxw2vpQzx4",
+  "https://unsplash.com/photos/LNn6O_Mt730",
+  "https://unsplash.com/photos/r01ZopTiEV8",
+];
+
+let slideShow = document.getElementById("image__carousel")
+slideShow.src = "https://images.unsplash.com/photo-1504113888839-1c8eb50233d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=829&q=80";
+
+
+let backwardButton = document.getElementById("backward__btn");
+let forwardButton = document.getElementById("forward__btn");
+
+
+
+// backwardButton.addEventListener("click", () {
+
+// });
