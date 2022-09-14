d08ebe66988b33f8d081de5382c1d12cf584d8f4
Gelson Gerson Celestino <103900087+GelsonGerson@users.noreply.github.com>
2022-09-14 17:01:31 +0200

    Exercise

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeDisplay = document.getElementById("timeRemaining");
+  let alarmInput = document.getElementById("alarmSet").value;
+
+  const interval = setInterval(() => {
+    let mm = Math.floor(alarmInput / 60);
+    let ss = alarmInput % 60;
+
+    mm = mm < 10 ? "0" + mm : mm;
+    ss = ss < 10 ? "0" + ss : ss;
+
+    timeDisplay.innerText = `Time Remaining: ${mm}:${ss}`;
+
+    alarmInput--;
+
+    if (alarmInput < 0) {
+      clearInterval(interval);
+      playAlarm();
+      let colours = ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"];
+
+      let colourInterval = setInterval(() => {
+        let colourIndex = Math.floor(Math.random() * colours.length);
+        backgroundSetColour = document.querySelector("body");
+        backgroundSetColour.style.backgroundColor = colours[colourIndex];
+      }, 1000);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,78 @@
 // Write your code here
+
+let imageCarouselArr = [
+  "https://images.unsplash.com/photo-1504113888839-1c8eb50233d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=829&q=80",
+  "https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGZvb2R8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGZvb2R8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1562059390-a761a084768e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZvb2R8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+];
+
+let backwardButton = document.getElementById("backward__btn");
+let forwardButton = document.getElementById("forward__btn");
+let autoBackwardBtn = document.getElementById("autoback__btn");
+let autoForwardBtn = document.getElementById("autoforward__btn");
+let stopBtn = document.getElementById("stop__btn");
+let autoForwardInterval;
+let autoBackInterval;
+let counter = 0;
+
+forwardButton.addEventListener("click", () => {
+  let slideShow = document.getElementById("image__carousel");
+  if (counter < imageCarouselArr.length - 1) {
+    counter++;
+    slideShow.src = imageCarouselArr[counter];
+    console.log(counter);
+  } else {
+    counter = 0;
+    slideShow.src = imageCarouselArr[counter];
+    console.log(counter);
+  }
+});
+
+backwardButton.addEventListener("click", () => {
+  let slideShow = document.getElementById("image__carousel");
+  if (counter > 0) {
+    counter--;
+    slideShow.src = imageCarouselArr[counter];
+    console.log(counter);
+  } else {
+    counter = imageCarouselArr.length - 1;
+    slideShow.src = imageCarouselArr[counter];
+    console.log(counter);
+  }
+});
+
+autoBackwardBtn.addEventListener("click", () => {
+  autoBackInterval = setInterval(() => {
+    let slideShow = document.getElementById("image__carousel");
+    if (counter > 0) {
+      counter--;
+      slideShow.src = imageCarouselArr[counter];
+      console.log(counter);
+    } else {
+      counter = imageCarouselArr.length - 1;
+      slideShow.src = imageCarouselArr[counter];
+      console.log(counter);
+    }
+  }, 3000);
+});
+
+autoForwardBtn.addEventListener("click", () => {
+  autoForwardInterval = setInterval(() => {
+    let slideShow = document.getElementById("image__carousel");
+    if (counter < imageCarouselArr.length - 1) {
+      counter++;
+      slideShow.src = imageCarouselArr[counter];
+      console.log(counter);
+    } else {
+      counter = 0;
+      slideShow.src = imageCarouselArr[counter];
+      console.log(counter);
+    }
+  }, 3000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(autoBackInterval);
+  clearInterval(autoForwardInterval);
+});
