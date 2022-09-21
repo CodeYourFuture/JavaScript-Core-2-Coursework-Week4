b78333ff1f2304fe23ea10a46f78d697304938d2
EleMashige <elekanyanimashige@yahoo.com>
2022-09-21 11:18:14 +0200

    updated

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+const display = document.getElementById('timeRemaining');
+
+function setAlarm() {
+  var today = new Date();
+  const hour = formatTime(today.getHours());
+  const minutes = formatTime(today.getMinutes());
+  const seconds = formatTime(today.getSeconds());
+  const now = `${hour}:${minutes}:${seconds}`;
+
+  display.innerText=`${hour}:${minutes}:${seconds}`;
+  
+//     check if the alarmList includes the current time , "now"
+//     if yes, ringing() is called
+  // if(alarmList.includes(now) ){
+  //     ringing(now);
+  // } 
+}
+
+// set the correct format of time
+// converts "1:2:3" to "01:02:03"
+function formatTime(time) {
+  if ( time < 10 && time.length != 2) {
+      return '0' + time;
+  }
+  return time;
+}
+
+
 
 // DO NOT EDIT BELOW HERE
 
