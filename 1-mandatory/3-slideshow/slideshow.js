05c3774e0e75afb2891abc3e4814dea6cb77ed4c
Anthony Mogotlane <almogotlane@gmail.com>
2022-09-28 15:59:00 +0200

    Completed level 1 challenge of the carousel

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,28 @@
 // Write your code here
+let images = [
+    "https://images.unsplash.com/photo-1650225502063-0e69901fad8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1650275140172-5269844b09ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1650205511636-12479f0133d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1650262673518-c31fa33dedf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1650288021655-28883d80e1bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+]
+
+const img = document.querySelector(".img");
+const prev = document.querySelector(".btn-prev");
+const next = document.querySelector(".btn-next");
+
+
+next.addEventListener("click", () => {
+    images.push(images[0]);
+    images.shift();
+    img.setAttribute("src", images[0]);
+})
+
+prev.addEventListener("click", () => {
+    images.unshift(images[images.length - 1]);
+    images.pop();
+    img.setAttribute("src", images[images.length - 1]);
+})
+
+
+
