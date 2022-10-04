ccbdf1c27c9e65f3b27d95ebc31efd0f66cc8efa
farzad-nazif <farzadnazif.nz@gmail.com>
2022-10-04 13:37:41 +0100

    1-mandatory/3-slideshow level 1 finished

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,20 @@
 // Write your code here
+let images = document.querySelector("#images");
+let nextBtn = document.querySelector("#next");
+let previousBtn = document.querySelector("#previous");
+let count = 1;
+images.setAttribute("src", "sample-images/image1.jpg");
+nextBtn.addEventListener("click", () => {
+  count += 1;
+  if (count > 4) {
+    count = 1;
+  }
+  images.setAttribute("src", `sample-images/image${count}.jpg`);
+});
+previousBtn.addEventListener("click", () => {
+  count -= 1;
+  if (count < 1) {
+    count = 4;
+  }
+  images.setAttribute("src", `sample-images/image${count}.jpg`);
+});
