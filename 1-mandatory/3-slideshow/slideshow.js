40b7f48f965f2e073629805acc68a2b0cbef3db7
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-04 17:25:30 +0100

    Added images

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,8 @@
-// Write your code here
+let imgArray = [
+  "https://images.unsplash.com/photo-1615412704911-55d589229864?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=982&q=80",
+  "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  "https://images.unsplash.com/photo-1614531341773-3bff8b7cb3fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+];
+
+let imgDiv = document.querySelector(".card__img");
+imgDiv.style.backgroundImage = `url(${imgArray[0]})`;
