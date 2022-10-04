5eb15e1e2440cbb6f9f8ceda92754cf12b69dfb8
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-04 22:09:03 +0100

    Refactoring solution

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,33 +1,33 @@
-let isPaused = false;
+let interval;
+let bgInterval;
 function setAlarm() {
-  let inputValue = document.getElementById("alarmSet").value;
-  let timeRemaining = document.getElementById("timeRemaining");
-  
-  setInterval(() => {
-    if (!isPaused) {
-      let seconds = inputValue % 60;
-      let minutes = Math.floor(inputValue / 60);
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      timeRemaining.innerText = `Time Remaining: ${minutes}:${seconds}`;
+  let input = document.getElementById("alarmSet");
+  let heading = document.getElementById("timeRemaining");
+  let timeLeft = input.value;
+  interval = setInterval(() => {
+    let minutes = Math.floor((timeLeft % (60 * 60)) / 60);
+    let seconds = Math.floor(timeLeft % 60);
 
-      if (inputValue < 0) {
-        clearInterval(setAlarm);
-        timeRemaining.innerText = "Time Remaining: 00:00";
-        playAlarm();
-        flashBg();
-      }
-      inputValue--;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
     }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+
+    if (timeLeft <= 0) {
+      clearInterval(interval);
+      heading.innerText = "Time Remaining: 00:00";
+      playAlarm();
+      flashBg();
+    }
+    timeLeft--;
   }, 1000);
 }
 
 function flashBg() {
-  setInterval(() => {
+  bgInterval = setInterval(() => {
     if (document.body.style.backgroundColor !== "yellow") {
       document.body.style.backgroundColor = "yellow";
     } else {
@@ -35,6 +35,7 @@ function flashBg() {
     }
   }, 20);
 }
+
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
@@ -51,12 +52,11 @@ function setup() {
 
 function playAlarm() {
   audio.play();
-  isPaused = false;
 }
 
 function pauseAlarm() {
   audio.pause();
-  isPaused = true;
+  clearInterval(bgInterval);
 }
 
 window.onload = setup;
