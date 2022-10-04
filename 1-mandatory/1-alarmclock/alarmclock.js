75dfa32fd59ce785430feab080a8f7984e130f26
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-04 12:30:47 +0100

    Added setAlarm function

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,19 @@
-function setAlarm() {}
+function setAlarm() {
+  let inputValue = document.getElementById("alarmSet").value;
+  let timeRemaining = document.getElementById("timeRemaining");
+  let seconds = inputValue % 60;
+  let minutes = Math.floor(inputValue / 60);
+
+  if (seconds < 10 && minutes < 10) {
+    timeRemaining.innerHTML = `Time Remaining: 0${minutes}:0${seconds}`;
+  } else if (seconds < 10 && minutes >= 10) {
+    timeRemaining.innerHTML = `Time Remaining: ${minutes}:0${seconds}`;
+  } else if (seconds >= 10 && minutes < 10) {
+    timeRemaining.innerHTML = `Time Remaining: 0${minutes}:${seconds}`;
+  } else {
+    timeRemaining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
