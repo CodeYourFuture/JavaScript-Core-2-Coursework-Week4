f9ba73de6d01ec9b2d6eaa2f3524fc16a5fcead6
Abdirahim Hussein <drahimhussein@gmail.com>
2022-10-04 14:32:23 +0100

    set constants for alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+const timeRemaining = document.querySelector("#timeRemaining");
+const alarmSetInput = document.querySelector("#alarm set")
+const setButton = document.querySelector("#set");
+const stopButton = document.querySelector("#stop");
+
+
+let inputValue
+let currentTime;
+
+let interval;
+
+function setAlarmInterval() {
+  if(!interval) interval = setInterval(setAlarm, 1000);
+}
+
+function setAlarm() {
+  if(inputValue >= 0) {
+    let hours = Math.floor(inputValue / 60);
+    let minutes = inputValue % 60;
+
+    timeRemaining.innerHTML = `<h1 id="timeRemaining">Time Remaining: ${hours}:${minutes}</h1>`;
+
+    inputValue -= 1;
+  }
 
 // DO NOT EDIT BELOW HERE
 
