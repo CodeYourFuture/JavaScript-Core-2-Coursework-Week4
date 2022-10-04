7614d0564fd60f72364085b68c58e89e2a530e64
Dewayne Sewell <dewaynesewell1@gmail.com>
2022-10-04 19:55:51 +0100

    submittal

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,21 @@
 function setAlarm() {}
+  let time = document.getElementById("alarmSet").value;
+
+  const interval = setInterval(() => {
+    const minutes = Math.floor(time / 60);
+    const formattedMin = minutes < 10 ? `0${minutes}` : minutes;
+    const seconds = time % 60;
+    const formattedSec = seconds < 10 ? `0${seconds}` : seconds;
+
+    document.getElementById(
+      "timeRemaining"
+    ).innerText = `Time Remaining: ${formattedMin}:${formattedSec}`;
+    time--;
+    if (time < 0) {
+      clearInterval(interval);
+      playAlarm();
+    }
+  }, 1000);
 
 // DO NOT EDIT BELOW HERE
 
--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,31 @@
 // Write your code here
+let i = 0;
+function imageForward() {
+  document.getElementById("image").src = images[i++];
+  if (i === images.length) {
+    i = 0;
+  }
+}
+
+function imageBack() {
+  if (i > 0) {
+    document.getElementById("image").src = images[i--];
+  }
+}
+
+document.getElementById("back").addEventListener("click", imageBack);
+// document.getElementById("stop").addEventListener();
+document.getElementById("forward").addEventListener("click", imageForward);
+
+const images = [
+  "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
+  "https://img.freepik.com/premium-photo/cake-with-white-cream-chocolate-drips-pomegranate-nuts-chocolate-decor_199620-5923.jpg?w=2000",
+  "https://img.taste.com.au/hbNtzI2Q/taste/2021/08/clinkers-cake-173208-2.jpg",
+];
+
+window.onload = () => {
+  const image = document.createElement("img");
+  image.setAttribute("src", images[0]);
+  image.setAttribute("id", "image");
+  document.getElementById("imageDisplay").appendChild(image);
+};
