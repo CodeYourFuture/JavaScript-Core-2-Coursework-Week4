db3eb71e4e44b8c8fe4d5e98a74cda3c993bdece
Dawit Abraha <akdawit1@gmail.com>
2022-10-04 21:47:21 +0100

    Completed mandatory 1

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,26 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeDisplay = document.getElementById("timeRemaining");
+  let inputTime = document.getElementById("alarmSet");
+  let seconds = inputTime.value;
+  inputTime.value = "";
+  timeDisplay.innerText = `Time Remaining: ${timeFormatter(seconds)}`;
+  let countDownInterval = setInterval(() => {
+    seconds--;
+    timeDisplay.innerText = `Time Remaining: ${timeFormatter(seconds)}`;
+    if (timeDisplay.innerText === "Time Remaining: 00:00") {
+      playAlarm();
+      clearInterval(countDownInterval);
+    }
+   }, 1000);
+}
 
+function timeFormatter(sec) {
+  let minutes = Math.floor(sec / 60);
+  let seconds = sec % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  return `${minutes}:${seconds}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
