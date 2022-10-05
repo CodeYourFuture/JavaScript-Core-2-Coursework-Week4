d32ca9f5ea13402e950bf9d90df66eba48dccf34
Kawa MANMI <kawamam2007@gmail.com>
2022-10-05 23:57:31 +0100

    First Mandatory has been done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,25 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeDisplay = document.getElementById("timeRemaining");
+  let alarmValue = document.getElementById("alarmSet").value;
+  let extraZero = "";
 
+  if (alarmValue > 0 && parseInt(alarmValue) === eval(alarmValue)) {
+    if (alarmValue < 10) extraZero = "0";
+    const timeInterval = setInterval(function () {
+      updateTime();
+    }, 1000);
+  }
+  function updateTime() {
+    timeDisplay.innerHTML = `Time Remaining: 00:${extraZero}${alarmValue}`;
+    if (alarmValue === 0) {
+      playAlarm();
+    } else {
+      alarmValue = alarmValue - 1;
+    }
+  }
+}
+
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
