39b7891c3825e7aaf9b88125be1d0b00c9c0171b
Chelle <dearmichelley@gmail.com>
2022-10-05 20:05:38 +0100

    alarm: refactored

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,47 +1,37 @@
 function setAlarm() {
-  let time = document.querySelector("#time");
-  start = document.querySelector("#set");
+  const time = document.querySelector("#time");
+  let i = Number(document.querySelector("#alarmSet").value);
 
-  start.addEventListener("click", () => {
-    let i = Number(document.querySelector("#alarmSet").value);
+  i === 0 ? (i = 60) : i;
 
-    const convertTime = (i) => {
-      let h = Math.floor(i / 3600);
-      let m = Math.floor((i % 3600) / 60);
-      let s = Math.floor((i % 3600) % 60);
+  const convertTime = (i) => {
+    let h = Math.floor(i / 3600);
+    let m = Math.floor((i % 3600) / 60);
+    let s = Math.floor((i % 3600) % 60);
+    let hToShow = h > 10 ? h : `0${h}`;
+    let mToShow = m >= 10 ? `:${m}:` : `:0${m}:`;
+    let sToShow = s >= 10 ? s : `0${s}`;
+    time.innerText = hToShow + mToShow + sToShow;
+  };
 
-      let hToShow = h > 10 ? `${h}:` : `0${h}:`;
-      let mToShow = m >= 10 ? m : `0${m}`;
-      let sToShow = s >= 10 ? `:${s}` : `:0${s}`;
+  convertTime(i);
 
-      time.innerText = hToShow + mToShow + sToShow;
-    };
+  const set = setInterval(() => {
+    i--;
     convertTime(i);
-    const set = setInterval(() => {
-      i--;
-      convertTime(i);
-      if (i === 0) {
-        playAlarm();
-        clearInterval(set);
-      }
-    }, 1000);
-
-    let stopBtn = document.querySelector("#stop");
-    stopBtn.addEventListener("click", () => {
+    if (i === 0) {
+      playAlarm();
       clearInterval(set);
-      time.innerText = `00:00`;
-    });
+    }
+  }, 1000);
 
-    // convertTime(i);
+  const stopBtn = document.querySelector("#stop");
+
+  stopBtn.addEventListener("click", () => {
+    clearInterval(set);
+    time.innerText = `00:00`;
   });
-  //get user input
-  //convert user input to hours, minutes, and seconds
-
-  //start countdown
-  //ring alarm when countdown finishes
-  // clear interval
 }
-console.log(setAlarm());
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
