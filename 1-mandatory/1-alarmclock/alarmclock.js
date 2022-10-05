2df487312d3953d4f3d723241b8586eb1c444e16
Alex Phillip <alexjoelphillip@gmail.com>
2022-10-05 18:49:58 +0100

    began mandatory 1 alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  // link to input field
+  let inputValue = document.querySelector("#alarmSet").value;
+  //link to title
+  let timeOnScreen = document.querySelector("#timeRemaining");
+  // link to stop button
+  let stopAlarm = document.querySelector("#stop");
+  // link to 'Set Alarm' button
+  let setAlarmBtn = document.querySelector("#set");
+  // format input value so = xx:xx
+    let seconds = inputValue;
+    let minutes = 0;
+    if (inputValue > 59) {
+      minutes = Math.floor(inputValue / 60);
+      seconds = inputValue % 60;
+    }
+  // add event listener ('click') to button and create function to display input value on screen
+  setAlarmBtn.addEventListener("click", () => {
+  // set interval so displayed value decreases by 1 every second
+    setInterval(function() {
+    timeOnScreen.innerHTML = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    if (seconds > 0) {
+      seconds--
+  // when input value reaches 0, play alarm
+    } else if (seconds === 0 && minutes > 0) {
+      minutes--;
+      seconds = 59;
+    } else {
+      playAlarm()
+    }
+  }, 1000);
+  });
+};
 
 // DO NOT EDIT BELOW HERE
 
