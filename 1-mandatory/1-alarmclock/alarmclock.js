8ce76efecba20897c7930f6a4418a8f19ce2b636
kavitappatil <patil.poem@gmail.com>
2022-10-05 12:45:58 +0100

    1-alarmclock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+function setAlarm() {
+  let alarmSetValue = document.getElementById("alarmSet").value;
+  let timeDisplay = document.getElementById("timeRemaining");
+ let interval = setInterval(() => {
+  //work out mm and ss from input given
+ let minutes =Math.floor(alarmSetValue / 60);
+ let seconds = alarmSetValue%60;
+ //adding a "0" in front if its >= 10.
+ minutes = minutes >= 10 ? minutes : "0" + minutes;
+ seconds = seconds >= 10 ? seconds : "0" + seconds;
+// console.log(`${minutes}:${seconds}`); checked its working
+
+//display time on DOM
+timeDisplay.innerText=`Time Remaining: ${minutes}:${seconds}`;
+
+    
+alarmSetValue--;
+// stop interval and play alarm
+if (alarmSetValue < 0) {
+  clearInterval(interval);
+  playAlarm();
+} 
+
+
+ },1000);
+
+}
 
 // DO NOT EDIT BELOW HERE
 
