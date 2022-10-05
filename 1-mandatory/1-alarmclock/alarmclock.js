b6afaa03a618eee0815bd45ced2c1b9b550a1850
Chelle <dearmichelley@gmail.com>
2022-10-05 19:45:51 +0100

    exercise1: complete

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,36 +1,38 @@
 function setAlarm() {
-  //let timeRemaining = document.getElementById("timeRemaining");
-  // let alarmInput = document.querySelector("#alarmSet");
   let time = document.querySelector("#time");
   start = document.querySelector("#set");
+
   start.addEventListener("click", () => {
-    let num = 3600;
+    let i = Number(document.querySelector("#alarmSet").value);
+
+    const convertTime = (i) => {
+      let h = Math.floor(i / 3600);
+      let m = Math.floor((i % 3600) / 60);
+      let s = Math.floor((i % 3600) % 60);
+
+      let hToShow = h > 10 ? `${h}:` : `0${h}:`;
+      let mToShow = m >= 10 ? m : `0${m}`;
+      let sToShow = s >= 10 ? `:${s}` : `:0${s}`;
+
+      time.innerText = hToShow + mToShow + sToShow;
+    };
+    convertTime(i);
     const set = setInterval(() => {
-      num--;
-      convertTime(num);
-      if (num === 0) {
+      i--;
+      convertTime(i);
+      if (i === 0) {
         playAlarm();
         clearInterval(set);
       }
-      let stopBtn = document.querySelector("#stop");
-      stopBtn.addEventListener("click", () => {
-        clearInterval(set);
-        time.innerText = `00:00`;
-      });
     }, 1000);
 
-    function convertTime(n) {
-      n = Number(n);
-      let h = Math.floor(n / 3600);
-      let m = Math.floor((n % 3600) / 60);
-      let s = Math.floor((n % 3600) % 60);
+    let stopBtn = document.querySelector("#stop");
+    stopBtn.addEventListener("click", () => {
+      clearInterval(set);
+      time.innerText = `00:00`;
+    });
 
-      let hToShow = h > 10 ? h : `0${h}:`;
-      let mToShow = m >= 10 ? m : `0${m}`;
-      let sToShow = s >= 10 ? `:${s}` : `:0${s}`;
-      time.innerText = hToShow + mToShow + sToShow;
-    }
-    return convertTime(num);
+    // convertTime(i);
   });
   //get user input
   //convert user input to hours, minutes, and seconds
