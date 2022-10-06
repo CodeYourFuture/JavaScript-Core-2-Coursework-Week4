76738bef8653dadf88a8b4e39149f8a9daa1af14
simeonbikov <bikovsem@gmail.com>
2022-10-06 19:19:29 +0100

    mandatory-1 extra

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,39 +1,57 @@
+const pauseBtn = document.getElementById("pause");
+const countdownDisplay = document.getElementById("timeRemaining");
+let isPaused = false;
+var audio = new Audio("alarmsound.mp3");
+let flashingBackground;
 
-document.getElementById("set").addEventListener("click", setAlarm);
+function pauseCountdown() {
+  if (isPaused) {
+    isPaused = false;
+    pauseBtn.innerText = "Pause";
+  } else {
+    isPaused = true;
+    pauseBtn.innerText = "Continue";
+  }
+}
 
 function setAlarm() {
   pauseAlarm();
+  isPaused = false;
+  pauseBtn.innerText = "Pause";
+
   let time = document.getElementById("alarmSet").value;
-  let countdownDisplay = document.getElementById("timeRemaining");
   let counter = setInterval(updateCountdown, 1000);
 
   function updateCountdown() {
-    minutes = Math.floor(time / 60);
-    seconds = time % 60;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
     minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    countdownDisplay.innerText = `Time Remaining: ${minutes}:${seconds}`;
-    time--;
-    if (time === -1) {
-      clearInterval(counter);
-      playAlarm();
+    if (!isPaused) {
+      countdownDisplay.innerText = `Time Remaining: ${minutes}:${seconds}`;
+      time--;
+      if (time === -1) {
+        clearInterval(counter);
+        playAlarm();
+        setup();
+        flashingBackground = setInterval(changeBgColor, 200);
+      }
     }
   }
 }
 
-
-// DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
-
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
-  });
+  }, {once: true});
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+  });
+
+  pauseBtn.addEventListener("click", () => {
+    pauseCountdown();
   });
 }
 
@@ -43,6 +61,14 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
+  clearInterval(flashingBackground);
+  document.body.style.backgroundColor = "white";
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor === "red" ?
+  document.body.style.backgroundColor = "white" :
+  document.body.style.backgroundColor = "red";
 }
 
 window.onload = setup;
