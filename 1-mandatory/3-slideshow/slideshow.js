c1637c4999f79b609bc668b19cc844667debfbdc
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-06 09:25:24 +0100

    slideshow

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,38 @@
 // Write your code here
+const carasoulElement = document.querySelectorAll(".carasoul")
+
+carasoulElement.forEach((carasoul, indx)=> {
+    carasoul.style.transform = `translateX(${indx * 100}%)`
+
+})
+
+let curCarasoul = 0;
+let maxCarasoul = carasoulElement.length - 1;
+const nextCarasoul = document.querySelector(".btn-forward");
+
+nextCarasoul.addEventListener("click", function(){
+    if (curCarasoul === maxCarasoul){
+        curCarasoul = 0;
+    }else{
+        curCarasoul++;
+    }
+
+    carasoulElement.forEach((carasoul, indx) => {
+      carasoul.style.transform = `translateX(${100 * (indx - curCarasoul)}%)`;
+    });   
+})
+
+const prevCarasoul = document.querySelector(".btn-previous");
+
+prevCarasoul.addEventListener("click", function () {
+  if (curCarasoul === 0) {
+    curCarasoul = maxCarasoul;
+  } else {
+    curCarasoul--
+  }
+
+  carasoulElement.forEach((carasoul, indx) => {
+    carasoul.style.transform = `translateX(${100 * (indx - curCarasoul)}%)`;
+  });
+
+});
