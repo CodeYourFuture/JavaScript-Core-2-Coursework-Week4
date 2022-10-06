cedb8da4d96cd33d71fa6f4ea11925f22bc86777
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-10-06 16:36:52 +0100

    alarm

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,8 +1,28 @@
-function setAlarm() {}
+function setAlarm() {
+  const inputField = document.getElementById("alarmSet");
+  const timer = inputField.value;
+
+  const heading = document.getElementById("TimeRemaining");
+  heading.innerText = `Time remaining:00:${timer}`;
+
+  const myInterval = setInterval(() => {
+    timer = timer - 1;
+    heading.innerText = `Time Remaining:00:${Timer}`;
+
+    if (timer === 0) {
+      clearInterval(myInterval);
+      //timer stops
+      playAlarm();
+    }
+    }, 1000);
+
+  }
+
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
+
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
@@ -14,9 +34,20 @@ function setup() {
   });
 }
 
+
+
+
 function playAlarm() {
   audio.play();
 }
+
+
+
+
+
+
+
+
 
 function pauseAlarm() {
   audio.pause();
