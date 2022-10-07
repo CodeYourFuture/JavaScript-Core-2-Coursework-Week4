ce806444865eca92189465e6bfe60a087bb8bcfa
kavitappatil <patil.poem@gmail.com>
2022-10-07 23:45:12 +0100

    3-slideshow exerces

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -13,3 +13,41 @@ document.getElementById('previous').addEventListener('click', () => {
   imageArray.unshift(imageArray.pop());
   imageEl.setAttribute("src", imageArray[0]);
 });
+document.getElementById('auto-back').addEventListener('click', () => {
+    let autoBackInterval=setInterval(()=>{
+        imageArray.unshift(imageArray.pop());
+        imageEl.setAttribute("src", imageArray[0]);
+        document.getElementById("stop").addEventListener("click", () => {
+            clearInterval(autoBackInterval);
+        });
+        document.getElementById("auto-forward").addEventListener("click", () => {
+            clearInterval(autoBackInterval);
+        });
+        document.getElementById("next").addEventListener("click", () => {
+            clearInterval(autoBackInterval);
+        });
+        document.getElementById("previous").addEventListener("click", () => {
+            clearInterval(autoBackInterval);
+        });
+  },5000)
+});
+
+document.getElementById('auto-forward').addEventListener('click', () => {
+    let autoForwardInterval=setInterval(()=>{
+        imageArray.push(imageArray.shift());
+        imageEl.setAttribute("src", imageArray[0]);
+        document.getElementById("stop").addEventListener("click", () => {
+            clearInterval(autoForwardInterval);
+        });
+        document.getElementById("auto-back").addEventListener("click", () => {
+            clearInterval(autoForwardInterval);
+        });
+        document.getElementById("next").addEventListener("click", () => {
+            clearInterval(autoForwardInterval);
+        });
+        document.getElementById("previous").addEventListener("click", () => {
+            clearInterval(autoForwardInterval);
+        });
+  },5000)
+
+});
