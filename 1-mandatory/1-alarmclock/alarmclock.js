0c52f46051ff51b89b4ca8c04ebebd380ab4aed7
micheale2021 <andemariammicheale@gmail.com>
2022-10-13 11:14:56 +0100

    alarmclock completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  seconds = inputField.value;
+  inputField.value = ""; //clear input field
+  countdown();
+  countdownInterval = setInterval(countdown, 1000);
+}
+const inputField = document.getElementById("alarmSet");
+let timeRemain = document.getElementById("timeRemaining");
+const setAlarmButton = document.getElementById("set");
+const stopAlarmButton = document.getElementById("stop");
+
+stopAlarmButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = "white";
+});
+let seconds, countdownInterval;
+// time format
+const timeFormatter = (num) => {
+  let minutes = Math.floor(num / 60);
+  let seconds = num % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  return `${minutes}:${seconds}`;
+};
+
+const countdown = () => {
+  timeRemain.innerText = `Time Remaining: ${timeFormatter(seconds)}`;
+  seconds--;
+  if (timeRemain.innerText === "Time Remaining: 00:00") {
+    document.body.style.backgroundColor = "aqua";
+    playAlarm();
+    clearInterval(countdownInterval);
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
