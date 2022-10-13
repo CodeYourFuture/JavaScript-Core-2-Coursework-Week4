58430936e276e7653e285f6a076713c0f6e6bf8e
Lunamzelo <lunamzelo@gmail.com>
2022-10-13 11:39:12 +0100

    completed alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,29 @@
-function setAlarm() {}
+let setAlarmButton = document.getElementById("set");
+let input = document.getElementById("alarmSet");
+let timeRemaining = document.getElementById("timeRemaining");
+let intervalId, inputValue;
+function setAlarm() {
+  inputValue = input.value;
+  input.value = "";
+  decrementTime();
+  intervalId = setInterval(decrementTime, 1000);
+}
+function decrementTime() {
+  timeRemaining.innerHTML = `Time Remaining: ${timeFormatter(inputValue)}`;
+  inputValue--;
+  if (timeRemaining.innerHTML === "Time Remaining: 00:00") {
+    playAlarm();
+    clearInterval(intervalId);
+  }
+}
+function timeFormatter(number) {
+  let minutes = Math.floor(number / 60);
+  let sec = number % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  sec = sec < 10 ? "0" + sec : sec;
 
+  return `${minutes}:${sec}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
