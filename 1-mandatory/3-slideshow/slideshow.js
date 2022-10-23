b96c3bd9f9f26ae62320d62fcc7e8b03a249999c
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-10-23 13:44:03 +0100

    complete mandatory challenges

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,62 @@
-// Write your code here
+let index = 0;
+let interval;
+
+const images = [
+  "https://c4.wallpaperflare.com/wallpaper/274/570/535/nature-trees-hills-mist-wallpaper-preview.jpg",
+  "https://wallpaperaccess.com/full/2960486.jpg",
+  "https://images.wallpaperscraft.com/image/single/hills_distance_sunset_183641_1920x1080.jpg",
+  "https://c4.wallpaperflare.com/wallpaper/437/34/474/sunlight-over-the-hills-wallpaper-preview.jpg",
+];
+
+const img = document.querySelector("img");
+img.setAttribute("src", images[index]);
+
+const btnNext = document.querySelector("#btn-next");
+const btnBack = document.querySelector("#btn-back");
+const btnStop = document.querySelector("#btn-stop");
+const btnAutoBackward = document.querySelector("#btn-auto-backward");
+const btnAutoForward = document.querySelector("#btn-auto-forward");
+
+btnNext.addEventListener("click", () => {
+  moveForward();
+});
+
+btnBack.addEventListener("click", () => {
+  moveBackward();
+});
+
+btnAutoForward.addEventListener("click", () => {
+  interval = setInterval(() => {
+    moveForward();
+  }, 2000);
+});
+
+btnAutoBackward.addEventListener("click", () => {
+  interval = setInterval(() => {
+    moveBackward();
+  }, 2000);
+});
+
+btnStop.addEventListener("click", () => {
+  clearInterval(interval);
+});
+
+function moveForward() {
+  if (index === images.length - 1) {
+    index = 0;
+    img.setAttribute("src", images[index]);
+  } else {
+    index++;
+    img.setAttribute("src", images[index]);
+  }
+}
+
+function moveBackward() {
+  if (index === 0) {
+    index = images.length - 1;
+    img.setAttribute("src", images[index]);
+  } else {
+    index--;
+    img.setAttribute("src", images[index]);
+  }
+}
