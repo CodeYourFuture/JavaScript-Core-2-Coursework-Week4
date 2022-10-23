bba1c0f1afba9d2b727ee6a90f8dba52b387e0b4
4maziar <53979524+8maziar@users.noreply.github.com>
2022-10-23 15:27:37 +0100

    alarmclock completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,46 @@
-function setAlarm() {}
+const timeRemaining = document.getElementById("timeRemaining");
+const alarmSet = document.getElementById("alarmSet");
+const stop = document.getElementById("stop");
+let isTimerOff = true;
+
+function setAlarm() {
+  let timeValue = alarmSet.value;
+  if (timeValue && /^[0-9]+$/g.test(timeValue) && timeValue > 1 && isTimerOff) {
+    isTimerOff = false;
+    timeRemaining.innerText = `Time Remaining: ${getCorrectTimeFormat(
+      timeValue
+    )}`;
+    // interval 1 second to show count down
+    const secondCount = setInterval(() => {
+      timeValue--;
+      timeRemaining.innerText = `Time Remaining: ${getCorrectTimeFormat(
+        timeValue
+      )}`;
+      if (timeValue < 1) {
+        playAlarm();
+        clearInterval(secondCount);
+        document.body.style.backgroundColor = "red";
+        isTimerOff = true;
+      }
+    }, 1000);
+  }
+  alarmSet.value = "";
+  // Stop the alarm clock
+  stop.addEventListener("click", () => window.location.reload());
+}
+
+// This function format the seconds to min and sec
+function getCorrectTimeFormat(timeValue) {
+  const addZeroToTime = (num) => (num < 10 ? "0" + num : num);
+  const editedTime =
+    timeValue < 60
+      ? `00:${addZeroToTime(timeValue)}`
+      : `${addZeroToTime(Math.floor(timeValue / 60))}:${addZeroToTime(
+          timeValue % 60
+        )}`;
+
+  return editedTime;
+}
 
 // DO NOT EDIT BELOW HERE
 
