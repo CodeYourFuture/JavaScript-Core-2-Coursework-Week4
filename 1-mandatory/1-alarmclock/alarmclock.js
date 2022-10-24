0ad8601bb5128866da698f499cfe10feb6f47351
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-10-24 07:19:31 +0100

    alarmClock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,26 @@
-function setAlarm() {}
+function setAlarm() {
+  let setAlarmButton=document.querySelector('#set');
+  let theCounter=document.querySelector('#timeRemaining');
+  let inputTime=document.querySelector('#alarmSet');
+  let seconds=inputTime.value;
+  theCounter.innerText=`Time Remaining: 00:${seconds}`;
+  // I want to writ down the countdown function
+  const countDown= setInterval(()=>{
+    seconds--;
+    theCounter.innerText=`Time Remaining: 00:${seconds}`;
+    console.log(seconds);
+    if(seconds===0){
+      playAlarm()
+      clearInterval(countDown)
+    }
+
+
+
+  },1000)
+
+  }
+  setAlarm();
+
 
 // DO NOT EDIT BELOW HERE
 
