5ef8dafa29f05a93e26d771de222d2a3ccdb36fe
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-10-24 23:35:36 +0100

    Mandatory 1 done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+let timer;
+
+function setAlarm() {
+  clearInterval(timer);
+  let inputVal = document.getElementById("alarmSet").value;
+  timer = setInterval(alarmCount, 1000);
+  function alarmCount() {
+    if (inputVal >= 0) {
+      document.getElementById(
+        "timeRemaining"
+      ).innerText = `Time Remaining: ${formatDisplay(inputVal)} `;
+      inputVal -= 1;
+      if (inputVal < 0) {
+        playAlarm();
+      }
+    }
+  }
+}
+
+function formatDisplay(timeEntry) {
+  let displayMins = Math.floor(timeEntry / 60)
+    .toString()
+    .padStart(2, "0");
+  let displaySecs = (timeEntry % 60).toString().padStart(2, "0");
+  return `${displayMins}:${displaySecs}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
