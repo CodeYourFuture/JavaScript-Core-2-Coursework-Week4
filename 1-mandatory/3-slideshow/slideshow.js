ab9a649647ba09c5d7610d23370b1745775296a4
Adibab <adiba.du.bd@gmail.com>
2022-10-25 22:35:33 +0100

    complet challenge 2 project slideshow

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,5 +1,5 @@
 // Write your code here
-
+// challenge 1
 const flowerArray = [
   "example-screenshots/1.flower1.jpg",
   "example-screenshots/2.flower2.jpg",
@@ -44,3 +44,42 @@ function prevPhoto() {
     image.src = flowerArray[counter];
   }
 }
+
+/* 
+challenge# 2
+
+ Make auto-forward and auto-back buttons to automatically move in that direction through the list of images.
+*/
+
+// select the btn to move forward / backward
+const btnAutoForward = document.getElementById("btn-auto-forward");
+const btnAutoBackward = document.getElementById("btn-auto-backward");
+const btnStop = document.getElementById("btn-stop");
+let time = 2000;
+
+// add event listener to auto forward
+btnAutoForward.addEventListener("click", function () {
+ forwardInterval = setInterval(()=> {
+nextPhoto()
+ }, time)})
+
+//   add event listerner to auto backward
+btnAutoBackward.addEventListener("click", function () {
+  backwardInterval = setInterval(() => {
+    prevPhoto();
+  }, time);
+});
+ // btn stop
+ btnStop.addEventListener("click", stop)
+ function stop (){
+  clearAutoimages()
+ }
+
+
+function clearAutoimages(){
+clearInterval(forwardInterval)
+//  clearauto
+// function clearAuto() {
+  //  clearInterval(forwardInterval);
+   clearInterval(backwardInterval);
+ }
