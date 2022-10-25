47eb642ceb88fc6ed8621a1c160350b33ff15234
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-10-25 09:59:59 +0100

    complete AlarmClock task

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,18 @@
-function setAlarm() {}
+function setAlarm() {
+  const clock = document.querySelector("#timeRemaining"); // select the H1 tag 
+  let time = document.getElementById("alarmSet").value; // get value from input and select by ID
+  let timer = setInterval(() => { // set timer every 1 sec
+    time--; // value - 1
+    if (time <= 0) { 
+      clearInterval(timer); // reset timer 
+      playAlarm(); // play alarm 
+    }
+    clock.innerHTML = `Time Remaining: ${Math.floor(time / 60) }:${( 
+      "0" +
+      (time % 60)
+    ).slice(-2)}`; // Display clock by hours and sec 
+  }, 1000 // set after 1 sec );
+}
 
 // DO NOT EDIT BELOW HERE
 
