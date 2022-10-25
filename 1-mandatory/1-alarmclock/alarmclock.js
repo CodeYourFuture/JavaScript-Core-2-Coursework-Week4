da3e71cd25ff8ef51752c89ca8847f29451a4e4a
Miladepour <epour.milad@gmail.com>
2022-10-25 13:02:12 +0100

    Alarm clock Done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,24 @@
-function setAlarm() {}
+function setAlarm() {
+  let stopAlarm = document.getElementById("stop");
+  let input = document.getElementById("alarmSet");
+  let remainTime = document.getElementById("timeRemaining");
+  let valueOfInput = input.value;
+
+  let timmer = setInterval(() => {
+    let minutes = Math.floor(valueOfInput / 60);
+    let seconds = valueOfInput % 60;
+    let minFormat = minutes < 10 ? `0${minutes}` : minutes;
+    let secFormat = seconds < 10 ? `0${seconds}` : seconds;
+    valueOfInput--;
+    remainTime.textContent = ` Time Remaining: ${minFormat}:${secFormat}`;
+    if (valueOfInput < 0) {
+      clearInterval(timmer);
+      playAlarm();
+    }
+  }, 1000);
+  input.value = "";
+  stopAlarm.addEventListener("click",pauseAlarm)
+}
 
 // DO NOT EDIT BELOW HERE
 
