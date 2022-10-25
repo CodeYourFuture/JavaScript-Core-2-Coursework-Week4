69c04b6ffb64663ff3ef7ae76ae5fede6f9a1a32
Doris-Siu <doris.siu.my@gmail.com>
2022-10-25 20:56:39 +0100

    Complete mandatory 3

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,31 @@
 // Write your code here
+const catsPic = [
+  "cats/cat0.jpg",
+  "cats/cat1.jpg",
+  "cats/cat2.jpg",
+  "cats/cat3.jpg",
+];
+let intervalId = null;
+let currentSlide = 0;
+
+const slideChange = (step) => {
+  currentSlide = currentSlide + step;
+  if (currentSlide === catsPic.length) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = catsPic.length - 1;
+  }
+  document.getElementById("image").setAttribute("src", catsPic[currentSlide]);
+};
+
+const autoPlay = (step) => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+  if (step != 0) {
+    intervalId = setInterval(() => {
+      slideChange(step);
+    }, 1000);
+  }
+};
