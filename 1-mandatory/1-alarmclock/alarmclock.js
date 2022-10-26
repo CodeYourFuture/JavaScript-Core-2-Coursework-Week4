ceb9941e49470b00c21062fb9ffa8b34a5e767b4
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2022-10-26 15:50:09 +0100

    alarmclock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,74 @@
-function setAlarm() {}
+const display = document.querySelector("#timeRemaining");
+var audio = new Audio("alarmsound.mp3");
+audio.loop = true;
+let alarmTime = null;// alarme time and alarmSet for me
+let alarmtimeout = null;
+
+
+function updateTime(){
+  const date = new Date();
+
+  const hour = formatTime(date.getHours());
+  const ninutes = formatTime(date.getMinutes());
+  const seconds = formatTime(date.getSeconds());
+
+  display.innerHTML = `${hour} : ${ninutes} : ${seconds}`;
+}
+
+function formatTime(time){
+if (time < 10){
+  return "0" + time;
+}
+return time;
+}
+
+function setAlarmTime(value){
+alarmTime = value;
+
+}
+
+function setAlarm (){
+  if (alarmTime){
+    const current = new Date();
+    const timeToAlarm = new Date(alarmTime);
+
+    if (timeToAlarm > current){
+      const timeout = timeToAlarm.getTime() - current.getTime();
+      alarmtimeout = setTimeout(() => audio.play(), timeout);
+    alert("Set Alarm");
+    }
+  }
+}
+
+function clearAlarm(){
+  audio.pause();
+  if (alarmtimeout){
+    clearTimeout(alarmtimeout);
+    alert("Alarm cleared");
+  }
+
+}
+
+
+
+
+
+
+setInterval(updateTime, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
@@ -23,3 +93,8 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
+/////////////////////////////////////////////////////////////
+
+
+
+
