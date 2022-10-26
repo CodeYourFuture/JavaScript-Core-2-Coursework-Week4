377cfb5562ee58d5047995af6f4c586cf7b30734
Miladepour <epour.milad@gmail.com>
2022-10-26 19:24:40 +0100

    Slideshow Level 2

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,33 +1,61 @@
 const images = [
-    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic1.jpg",
-    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic2.jpg",
-    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic3.jpg",
-    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic4.jpg",
-]
+  "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic1.jpg",
+  "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic2.jpg",
+  "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic3.jpg",
+  "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic4.jpg",
+];
 
-
-const slideShow = document.getElementById("slideShow")
-const backBtn = document.getElementById("backBtn")
-const forwardBtn = document.getElementById("forwardBtn")
+const slideShow = document.getElementById("slideShow");
+const backBtn = document.getElementById("backBtn");
+const forwardBtn = document.getElementById("forwardBtn");
+const autoBackBtn = document.getElementById("autoBackBtn");
+const autoForwardBtn = document.getElementById("autoForwardBtn");
+const stopBtn = document.getElementById("stopBtn");
 let slider = 0;
+let myIntervalForward;
+let myIntervalBack;
 
-window.onload = forward
+window.onload = forward;
 
-forwardBtn.addEventListener("click",forward)
-function forward(){
-    slideShow.src = images[slider];
-    slider++;
-    if (slider === images.length) {
-        slider = 0;
-    }
-}
-backBtn.addEventListener("click",back)
-function back(){
-    slider--;
-    slideShow.src = images[slider];
-    if (slider === images.length) {
-        slider = 0;
-    }
+forwardBtn.addEventListener("click", forward);
+function forward() {
+  slideShow.src = images[slider];
+  slider++;
+  if (slider > images.length -1) {
+    slider = 0;
+  }
 }
 
+backBtn.addEventListener("click", back);
+function back() {
+  slider--;
+  slideShow.src = images[slider];
+  if (slider === 0) {
+    slider = images.length -1;
+  }
+}
 
+autoForwardBtn.addEventListener("click", autoForward);
+
+function autoForward() {
+    stopSlide()
+    myIntervalForward = setInterval((callback) => {
+    callback = forward();
+  }, 2000);
+}
+
+autoBackBtn.addEventListener("click",autoBack)
+
+function autoBack() {
+    stopSlide()
+   myIntervalBack = setInterval((callback) => {
+    callback = back();
+  }, 2000);
+}
+
+stopBtn.addEventListener("click", stopSlide);
+
+function stopSlide() {
+  clearInterval(myIntervalBack);
+  clearInterval(myIntervalForward);
+}
