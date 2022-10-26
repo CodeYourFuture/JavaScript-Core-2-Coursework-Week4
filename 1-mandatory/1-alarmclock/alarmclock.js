a4222d598d300d6066309c6755c7e4304c99aca6
Ekrem <ekremteke.uk@gmail.com>
2022-10-26 20:36:06 +0100

    mandatory 1 was completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+let seconds = null;
+function setAlarm() {
+  let timeRemaining = document.getElementById("timeRemaining");
+  let input = document.getElementById("alarmSet");
+  let inputValue = input.value;
+  seconds = setInterval(() => {
+    let minutes = Math.floor(inputValue / 60);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let second = inputValue % 60;
+    second = second < 10 ? "0" + second : second;
+    // minutes = minutes < 10 ? "0" + minutes : minutes;
+    timeRemaining.innerHTML = "Time Remaining: " + minutes + ":" + second;
+
+    inputValue--;
+
+    if (inputValue <= 0) {
+      playAlarm();
+      clearInterval(seconds);
+      timeRemaining.innerHTML = "Time Remaining: 00:00";
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
