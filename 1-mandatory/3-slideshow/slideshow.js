fb86b03d61918f19d6bca6856311557e75b50c0d
Miladepour <epour.milad@gmail.com>
2022-10-26 15:31:03 +0100

    Slideshow Level 1

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,33 @@
-// Write your code here
+const images = [
+    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic1.jpg",
+    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic2.jpg",
+    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic3.jpg",
+    "https://cynoco.co.uk/wp-content/uploads/2022/10/Pic4.jpg",
+]
+
+
+const slideShow = document.getElementById("slideShow")
+const backBtn = document.getElementById("backBtn")
+const forwardBtn = document.getElementById("forwardBtn")
+let slider = 0;
+
+window.onload = forward
+
+forwardBtn.addEventListener("click",forward)
+function forward(){
+    slideShow.src = images[slider];
+    slider++;
+    if (slider === images.length) {
+        slider = 0;
+    }
+}
+backBtn.addEventListener("click",back)
+function back(){
+    slider--;
+    slideShow.src = images[slider];
+    if (slider === images.length) {
+        slider = 0;
+    }
+}
+
+
