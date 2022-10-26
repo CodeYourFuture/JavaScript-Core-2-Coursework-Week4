03f5cb43a6a313059d60ec0b910a90c8eaf46d3e
baharbazargan62 <baharbazargan62@gmail.com>
2022-10-26 15:33:43 +0100

    alarm-clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,7 +1,36 @@
-function setAlarm() {}
 
-// DO NOT EDIT BELOW HERE
-
+function setAlarm() {
+  let setAlarmButton=document.querySelector('#set');
+  let theCounter=document.querySelector('#timeRemaining');
+  let inputTime=document.querySelector('#alarmSet');
+  let seconds=inputTime.value;
+ 
+  const countDown= setInterval(()=>{
+    if (seconds<=60){
+    seconds--;
+    if(seconds<10){
+      theCounter.innerText=`Time Remaining: 00:0${seconds}`
+    }else theCounter.innerText=`Time Remaining: 00:${seconds}`;
+   }
+   
+    if(seconds===0){
+      playAlarm()
+      clearInterval(countDown)
+    }
+    if (seconds>59){
+      let min=Math.floor(seconds/60);
+      let secondnew=(seconds % 60)
+      seconds--;
+      if(secondnew<10){
+        theCounter.innerText=`Time Remaining: ${min}:0${secondnew}`
+      }else
+      theCounter.innerText=`Time Remaining: ${min}:${secondnew}`
+       
+     }
+    },1000)
+ 
+  }
+ 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
