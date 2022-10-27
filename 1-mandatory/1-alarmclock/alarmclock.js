9f0bbaa442a3143639539948daac58ee96a57c8b
fatih-celebi <celebifatih.c1@gmail.com>
2022-10-27 22:59:21 +0100

    Complete Alarmclock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,20 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeRemaining = document.getElementById("timeRemaining");
+  let input = document.getElementById("alarmSet");
+  let inputValue = input.value;
+  countdown = setInterval(() => {
+    let minutes = Math.floor(inputValue / 60);  
+    let seconds = inputValue % 60;
+    timeRemaining.innerText = `Time remaining: ${minutes}:${seconds}`;
+    if (inputValue === 0) {
+      playAlarm();
+      clearInterval(countdown);
+      timeRemaining.innerText = `Time remaining: ${minutes}:${seconds}`;
+    }
+    inputValue--;
+  }, 1000);
+  
+}
 
 // DO NOT EDIT BELOW HERE
 
