295f42e873f614f068c41d337e028088576cfbfa
Adibab <adiba.du.bd@gmail.com>
2022-10-27 12:16:21 +0100

    complete alarm clock project

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,20 @@
-function setAlarm() {}
+function setAlarm() {
+  // get the element to show ouput
+  const timeRemaining = document.getElementById("timeRemaining");
+  document.querySelector("body").style.backgroundColor = "white";
+  //  get the input value
+  let alarmSet = document.getElementById("alarmSet").value;
+  const countDown = setInterval(() => {
+    alarmSet--;
+    timeRemaining.innerText = `Time Remaining: 00:${alarmSet}`;
+    console.log(alarmSet);
+    if (alarmSet === 0) {
+      playAlarm();
+      document.querySelector("body").style.backgroundColor = "red";
+      clearInterval(countDown);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -10,7 +26,9 @@ function setup() {
   });
 
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
+    document.querySelector("body").style.backgroundColor = "black";
+  pauseAlarm();
+   
   });
 }
 
@@ -18,8 +36,9 @@ function playAlarm() {
   audio.play();
 }
 
-function pauseAlarm() {
-  audio.pause();
+ function pauseAlarm() {
+audio.pause();
+ clearInterval(countDown)
 }
 
 window.onload = setup;
