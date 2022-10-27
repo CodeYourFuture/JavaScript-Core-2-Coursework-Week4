1afa30634a978fb87f9c948d53118b1dc75623b9
Zaw Khing <zawmyokhing@gmail.com>
2022-10-27 16:28:52 +0100

    Complete alaramclock exercise

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,6 +1,26 @@
-function setAlarm() {}
+ 
+function setAlarm() {
+  let displayTime = document.getElementById("alarmSet").value;
+   
+   let myInterval = setInterval(function(){
+     if(displayTime === 0){
+          audio.play(clearInterval(myInterval));
+      }
+      document.getElementById("timeRemaining").innerHTML = `Time Remaining: 00:${displayTime}`;
+      displayTime--;
+   },1000)
+ }
+document.getElementById("set").addEventListener("click",setAlarm)
+
+
+function stopAlarm() {
+  clearInterval(myInterval);
+}
+document.getElementById("stop").addEventListener("click",stopAlarm)
+
 
 // DO NOT EDIT BELOW HERE
+
 
 var audio = new Audio("alarmsound.mp3");
 
--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,16 @@
 // Write your code here
+const image = document.querySelector("#images")
+image.setAttribute("src","button");
+
+btn-back.addEventListener('click',btnBack)
+function btnBack(){
+ let index = 0;
+ image.innerHTML = `${index[0]}`
+ index--;
+}
+
+btn-forward.addEventListener('click',btnForward)
+function btnForward(){
+  let index = 0;
+  index++;
+}
