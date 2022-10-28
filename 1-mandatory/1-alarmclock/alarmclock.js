3fabefaf1343f621e465728c1f49973b6fc4a7a5
Anosh Malik <anoshmalik.am@gmail.com>
2022-10-28 12:48:17 +0100

    Refactored code after reviewing other people's solutions

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,50 @@
-function setAlarm() {}
+
+  
+//  const inputTag = document.getElementById("alarmSet");
+//  const timer = document.getElementById("timeRemaining");
+//  let inputValue = 0;
+
+// function setAlarm() {
+//   inputValue = parseInt(inputTag.value);
+//   // for (let x = inputValue; x > -1; x--) {
+//     setTimeout(delays, 1000, inputValue);
+    
+//   };
+
+ 
+// // }
+
+// function delays(x) { 
+
+//   timer.innerText = "Time REMAINING => " + x;
+//   console.log(timer.innerText);
+     
+  
+
+//   }
+
+
+function setAlarm() {
+ 
+
+  let timeSetInput = document.querySelector("#alarmSet");
+  let remainder = document.querySelector("#timeRemaining");
+  let countDown = timeSetInput.value;
+
+  let timer = setInterval(() => {
+    const min = Math.floor(countDown / 60);
+    const minFormat = min < 10 ? `0${min}` : min;
+    const secs = countDown % 60;
+    const secFormat = secs < 10 ? `0${secs}` : secs;
+    remainder.textContent = `Time Remaining : ${minFormat}:${secFormat}`;
+    countDown--;
+
+    if (countDown < 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
