c4b1825e97b5d6df0bb5fb47d57cfc3986b032c7
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-11-02 15:12:01 +0000

    alarmclock completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,6 +1,31 @@
 function setAlarm() {
-  const SetAlarm = {}
+  // const SetAlarm = {}
+
+let timeRemain = getElementById('timeRemaining');
+let setTime = getElementById('alarmSet');
+let timer = timeRemain.value;
+
+let Timer = setInterval(() => {
+  
+  const hours = Math.floor(timer / 3600);
+  const hFormat = hours < 10 ? `0${hours}` : hours;
+  const minutes = Math.floor(timer / 60);
+  const mFormat = minutes < 10 ? `0${minutes}` : minutes;
+  const seconds = Math.floor(timer % 60);
+  const sFormat = seconds < 10 ? `0${seconds}` : seconds;
+   timeRemain.textContent = `Time Remaining : ${hFormat}: ${mFormat}: ${sFormat}`; 
+
+   if (timer < 0) {
+      clearInterval(Timer);
+      playAlarm();
+    }
+}, 1000);
+
+
 }
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
