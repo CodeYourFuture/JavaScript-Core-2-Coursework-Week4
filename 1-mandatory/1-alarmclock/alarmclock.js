12ea19fd88a12517b9852e76c0c83dfa7e8665c9
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2022-11-03 18:42:04 +0000

    exercise complete

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+function setAlarm() {
+  const inputTime = document.getElementById("alarmSet").value;
+  const timeRemaining = document.getElementById("timeRemaining");
+  /*function inputConvert(num) { 
+    const hours = Math.floor(num / 60);  
+    const minutes = num % 60;
+    return  hours + ":" + minutes; ;       
+}*/
+//if (inputTime < 60) {
+  // timeRemaining.innerText = `Time Remaining: 00:${inputTime}`;
+//}
+//else {
+  //const result = inputConvert(inputTime);
+  //return timeRemaining.innerText =  `Time Remaining : ${result}`;
+//}
+
+//if (result === 0) {
+  //playAlarm();
+//}
+
+setInterval(function() {
+  var hours = Math.floor((inputTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((inputTime % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((inputTime % (1000 * 60)) / 1000);
+  return timRemaining.innerText = `Time Remaining: ${hours}:${minutes}:${seconds}`;
+
+  // If the count down is finished, write some text
+
+  if (inputTime < 0) {
+    playAlarm();
+  }
+}, 1000);
+
+}
 
 // DO NOT EDIT BELOW HERE
 
