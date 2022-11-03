30a9169c3bb8cab31345717bdc6bcc6df12e4389
khmdagal <khmdagal@gmail.com>
2022-11-03 10:55:03 +0000

    Refactor and Make small changes

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,22 @@
 function setAlarm() {
   const setAlarmButton = document.querySelector("#set");
   let displayTime = document.querySelector("#second");
-
+  document.body.style.backgroundColor = "lightblue";
   setAlarmButton.addEventListener("click", function () {
-    const inputValue = document.querySelector("#alarmSet").value;
-    displayTime.innerHTML = inputValue;
+    let seconds = document.querySelector("#alarmSet").value;
 
-    setInterval(() => {
-      if (displayTime.innerHTML > 0) {
-       document.body.style.backgroundColor = "green";
-        displayTime.innerHTML--;
-        
-      } else {
-        displayTime = 0;
-        document.body.style.backgroundColor = "orange";
+    const countdown = setInterval(() => {
+      seconds--;
+      displayTime.innerHTML = seconds;
+      console.log(seconds);
+
+      if (seconds === 0) {
+        playAlarm();
+        document.body.style.backgroundColor = "lightgreen";
+        clearInterval(countdown);
       }
     }, 1000);
-
-
   });
-
 }
 
 setAlarm();
