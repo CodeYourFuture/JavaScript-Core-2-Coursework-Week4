24f1635b3ca75087f822edad123aa89659c070bf
hayatu4islam <hayatu4islam@yahoo.com>
2022-12-20 11:11:32 +0000

    Completed Level 1

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,27 @@
 // Write your code here
+const archImages = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+  "images/6.jpg"
+];
+
+// Creating other buttons
+let index = 0;
+document.getElementById("archImage").src = archImages[index];
+document.getElementById("forwBtn").addEventListener("click", () => {
+    index++;
+    if(index < archImages.length){
+        document.getElementById("archImage").src = archImages[index];
+    }
+    
+})
+
+document.getElementById("backBtn").addEventListener("click", () => {
+  index--;
+  if (index >= 0) {
+    document.getElementById("archImage").src = archImages[index];
+  }
+});
