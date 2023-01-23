0adf64420acaaa9b74d9f826e9ba9e5cfdb1ae47
nataliizab <nataliia.zab@gmail.com>
2023-01-23 18:13:27 +0000

    in process of solving task1

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+const changeTime = document.getElementById("timeRemaining");
+let innerTextOfTimer = changeTime.innerText.substr(16, 5);
+let minutes = +innerTextOfTimer.substr(0, 2);
+let seconds = +innerTextOfTimer.substr(3, 2);
+
+let timer = setInterval(function countDown() {
+  seconds = +seconds - 1;
+  changeTime.innerText = `Time Remaining: ${minutes}:${seconds}`;
+  // console.log(typeof minutes);
+  console.log(seconds);
+}, 1000);
+
+function setAlarm() {
+  const input = document.getElementById("alarmSet").value;
+  const changeTime = document.getElementById("timeRemaining");
+  let minutes = "00";
+  let seconds = "00";
+  if (input.toString().length <= 2) {
+    seconds = input.toString().padStart(2, "0");
+  } else if (input.toString().length > 2) {
+    minutes = input.toString().substr(0, 2);
+    seconds = input.toString().substr(2, 2).padEnd(2, "0");
+  }
+  changeTime.innerText = `Time Remaining: ${minutes}:${seconds}`;
+
+  countDown();
+}
+
+// 1234
+// When you click the `Set Alarm` button the counter at the top of the screen should
+// change to the number you entered in the `input` field. For example, if the `input`
+// field says `10` then the title should say `Time Remaining: 00:10`.
+
+// Every one second the title should count down by one.
 
 // DO NOT EDIT BELOW HERE
 
