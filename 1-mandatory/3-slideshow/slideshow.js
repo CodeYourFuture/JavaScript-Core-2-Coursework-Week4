fc1aa0238ada843dda0c2278100d7440586ea9ae
Hengameh M.Khani <108895187+HeniMKH@users.noreply.github.com>
2023-01-24 23:30:56 +0000

    mandatory

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,90 @@
 // Write your code here
+// level 1 challenge
+
+// assign my src codes
+const picturesSources = [
+"/1-mandatory/3-slideshow/images/1.jpg",
+"/1-mandatory/3-slideshow/images/2.jpg",
+"/1-mandatory/3-slideshow/images/3.jpg",
+"/1-mandatory/3-slideshow/images/4.jpg",
+"/1-mandatory/3-slideshow/images/5.jpg",
+
+];
+
+// get elements
+let images = document.getElementById("images")
+
+
+const forwardButton = document.getElementById("forward")
+const backButton = document.getElementById("back");
+
+let currentIndex = 0;
+
+
+function changePicturesForward() {
+currentIndex++;
+images.src = picturesSources[currentIndex % picturesSources.length];
+}
+
+function changePicturesBack() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = picturesSources.length - 1;
+  }
+  images.src = picturesSources[currentIndex % picturesSources.length];
+}
+
+forwardButton.addEventListener("click", changePicturesForward);
+
+backButton.addEventListener("click", changePicturesBack);
+
+
+// level 2 challenge 
+const intervalInput = document.getElementById("interval-input");
+let intervalId;
+let isPlaying = false;
+
+function forwardSlideshow() {
+  if ((intervalInput.value === "") || (intervalInput.value <= 0)) {
+    alert("Please enter a valid value and above 0");
+    return;
+  }
+    intervalId = setInterval(
+      changePicturesForward,
+      intervalInput.value * 1000
+    );
+isPlaying = true;
+autoForwardButton.disabled = true
+autoBackButton.disabled = true;
+intervalInput.value = ''
+}
+
+function backwardSlideshow() {
+  if ((intervalInput.value === "") || (intervalInput.value <= 0)) {
+    alert("Please enter a valid value and above 0");
+    return;
+  }
+  intervalId = setInterval(changePicturesBack, intervalInput.value * 1000);
+  isPlaying = true;
+  autoBackButton.disabled = true;
+  autoForwardButton.disabled = true;
+  intervalInput.value = "";
+}
+
+function stopSlideshow() {
+clearInterval(intervalId);
+isPlaying = false;
+autoForwardButton.disabled = false;
+autoBackButton.disabled = false;
+}
+
+
+const autoForwardButton = document.getElementById("auto-forward");
+const autoBackButton = document.getElementById("auto-back");
+const stopButton = document.getElementById("stop");
+
+autoForwardButton.addEventListener("click", forwardSlideshow);
+stopButton.addEventListener("click",stopSlideshow)
+autoBackButton.addEventListener("click", backwardSlideshow);
+
+
