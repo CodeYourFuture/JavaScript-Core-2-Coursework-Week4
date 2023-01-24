b94c3dc7ba140b69ab4443cd76a1d6f5f1274837
Mo Mahboobian <momahboobian@gmail.com>
2023-01-24 17:18:30 +0000

    mandatory 1

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,8 +1,34 @@
-function setAlarm() {}
+let remainingTime;
+let intervalId;
+
+function setAlarm() {
+  const alarmSetValue = document.getElementById("alarmSet").value;
+  remainingTime = alarmSetValue;
+  let minutes = Math.floor(remainingTime / 60);
+  let seconds = remainingTime % 60;
+  document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${
+    minutes < 10 ? "0" : ""
+  }${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  intervalId = setInterval(countdown, 1000);
+}
+
+function countdown() {
+  if (remainingTime > 0) {
+    remainingTime--;
+    let minutes = Math.floor(remainingTime / 60);
+    let seconds = remainingTime % 60;
+    document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${
+      minutes < 10 ? "0" : ""
+    }${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  } else {
+    clearInterval(intervalId);
+    playAlarm();
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+const audio = new Audio("alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
