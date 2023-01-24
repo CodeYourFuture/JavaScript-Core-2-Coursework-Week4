d0c9a67d1617559b47e9368234e7d72b7b2200fb
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-01-24 19:31:41 +0000

    alarmClock done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,44 @@
-function setAlarm() {}
+function setAlarm() {
+  let second = document.querySelector("#alarmSet").value;
+  let timeRemaining = document.querySelector("#timeRemaining");
+  let setButton = document.querySelector("#set");
+
+  let minute = parseInt(second / 60);
+  second %= 60;
+
+  setButton.addEventListener("click", () => {
+
+    setInterval(() => {
+
+      if (second === -1 && minute > 0) {
+        minute--;
+        second = 59;
+      }
+
+      if (second < 10)
+        second = second.toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false
+        })
+
+      if (minute < 10)
+        minute = minute.toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false
+        })
+
+      if (second >= 0) {
+        timeRemaining.innerText = `Time Remaining: ${minute}:${second}`;
+        second--;
+      }
+
+      if (second === -1 && minute === "00")
+        playAlarm()
+
+    }, 1000);
+  })
+
+}
 
 // DO NOT EDIT BELOW HERE
 
