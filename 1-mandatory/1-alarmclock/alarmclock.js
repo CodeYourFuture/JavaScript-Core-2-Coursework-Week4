4112139cc1803c700dd67190ce8a54a87f589b4f
Andriana <andrianasaffo@gmail.com>
2023-01-24 07:30:30 +0000

    Exercise 1 alarm clock completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,33 @@
-function setAlarm() {}
+function setAlarm() {
+  const timeInput = document.querySelector("#alarmSet");
+
+  let timeRemaining = document.querySelector("#timeRemaining");
+  let countdownTime = timeInput.value;
+
+  let minutes = Math.floor(countdownTime / 60);
+
+  let seconds = countdownTime - minutes * 60;
+
+  timeRemaining.innerText = `Time Remaining: 0${minutes}:${seconds}`;
+
+  let countdown = setInterval(() => {
+    seconds -= 1;
+    if (seconds == 0) {
+      clearInterval(countdown);
+
+      playAlarm();
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+      // minutes = "0" + minutes;
+    } else {
+      seconds = seconds;
+      minutes = minutes;
+    }
+
+    timeRemaining.innerText = `Time Remaining: 0${minutes}:${seconds}`;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
