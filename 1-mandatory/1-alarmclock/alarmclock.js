1a2ebd95465814bb61744070c8da91b7096d120f
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-01-25 00:16:15 +0000

    alarmClock-extra done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -2,13 +2,14 @@ function setAlarm() {
   let second = document.querySelector("#alarmSet").value;
   let timeRemaining = document.querySelector("#timeRemaining");
   let setButton = document.querySelector("#set");
+  const flash = ["red", "white"];
 
   let minute = parseInt(second / 60);
   second %= 60;
 
   setButton.addEventListener("click", () => {
 
-    setInterval(() => {
+    const stopTimeRemaining = setInterval(() => {
 
       if (second === -1 && minute > 0) {
         minute--;
@@ -32,12 +33,19 @@ function setAlarm() {
         second--;
       }
 
-      if (second === -1 && minute === "00")
+      if (second === -1 && minute === "00") {
+        let i = 0;
+        const stopFlashing = setInterval(() => {
+          document.body.style.backgroundColor = flash[i++ % flash.length];
+        }, 50)
         playAlarm()
-
+        clearInterval(stopTimeRemaining);
+        document.querySelector("#stop").addEventListener("click", () => {
+          clearInterval(stopFlashing);
+        })
+      }
     }, 1000);
   })
-
 }
 
 // DO NOT EDIT BELOW HERE
