1f5ecf173ae10833ea55d67d04d977f45a2d949d
Lorena <lorenacapraru@gmail.com>
2023-01-25 17:22:44 +0000

    setAlarm() done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,22 @@
-function setAlarm() {}
+function setAlarm() {
+  let inputTime = document.getElementById("alarmSet");
+  let timeRemain = document.getElementById("timeRemaining");
+  if (inputTime.value.length === 1)
+    timeRemain.innerHTML = `Time Remaining: 00:0` + inputTime.value;
+  else if (inputTime.value.length === 2 && Number(inputTime.value) < 60)
+    timeRemain.innerHTML = `Time Remaining: 00:` + inputTime.value;
+  else if (inputTime.value.length >= 2 && Number(inputTime.value) >= 60) {
+    let remaining = Number(inputTime.value) % 60;
+    let quotient = Math.floor(Number(inputTime.value / 60));
+    if (remaining <= 9)
+      timeRemain.innerHTML = `Time Remaining: 0${quotient}:0${remaining}`;
+    else timeRemain.innerHTML = `Time Remaining: 0${quotient}:${remaining}`;
+  }
+  inputTime.value = "";
+}
+
+/*let countDoawnFunction = setInterval(setAlarm()=>{
+}, 1000);*/
 
 // DO NOT EDIT BELOW HERE
 
