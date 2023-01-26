4a7bd9c7520de9acd5a27eab66edd647fd58c23b
Lorena <lorenacapraru@gmail.com>
2023-01-26 14:04:51 +0000

    Initial changes

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,71 @@
+//setInterval(setAlarm(), 1000);
+
 function setAlarm() {
   let inputTime = document.getElementById("alarmSet");
   let timeRemain = document.getElementById("timeRemaining");
-  if (inputTime.value.length === 1)
-    timeRemain.innerHTML = `Time Remaining: 00:0` + inputTime.value;
-  else if (inputTime.value.length === 2 && Number(inputTime.value) < 60)
-    timeRemain.innerHTML = `Time Remaining: 00:` + inputTime.value;
-  else if (inputTime.value.length >= 2 && Number(inputTime.value) >= 60) {
-    let remaining = Number(inputTime.value) % 60;
-    let quotient = Math.floor(Number(inputTime.value / 60));
-    if (Number(inputTime.value) >= 600 && remaining > 9)
-      timeRemain.innerHTML = `Time Remaining: ${quotient}:${remaining}`;
-    else if (Number(inputTime.value) >= 600 && remaining <= 9)
-      timeRemain.innerHTML = `Time Remaining: ${quotient}:0${remaining}`;
-    else if (Number(inputTime.value) < 600 && remaining > 9)
-      timeRemain.innerHTML = `Time Remaining: 0${quotient}:${remaining}`;
-    else if (Number(inputTime.value) < 600 && remaining <= 9)
-      timeRemain.innerHTML = `Time Remaining: 0${quotient}:0${remaining}`;
+  setInputToTitle();
+  if (timeRemain.innerText.includes("00:01")) playAudio();
+}
+
+function setInputToTitle() {
+  let inputTime = document.getElementById("alarmSet");
+  let timeRemain = document.getElementById("timeRemaining");
+  let remaining = Number(inputTime.value) % 60;
+  let quotient = Math.floor(Number(inputTime.value / 60));
+  if (quotient > 9 && remaining > 9) {
+    let interval = setInterval(function () {
+      timeRemain.innerHTML = `Time Remaining: ${quotient}:${remaining--}`;
+      if (remaining === 0) {
+        quotient--;
+        remaining = 59;
+      }
+    }, 1000);
+  } else if (quotient > 9 && remaining <= 9) {
+    interval = setInterval(function () {
+      timeRemain.innerHTML = `Time Remaining: ${quotient}:0${remaining--}`;
+      if (remaining === 0) {
+        quotient--;
+        remaining = 59;
+      }
+    }, 1000);
+  } else if (quotient <= 9 && remaining > 9) {
+    interval = setInterval(function () {
+      timeRemain.innerHTML = `Time Remaining: 0${quotient}:${remaining--}`;
+      if (remaining === 0) {
+        quotient--;
+        remaining = 59;
+      }
+    }, 1000);
+  } else if (quotient <= 9 && remaining <= 9) {
+    interval = setInterval(function () {
+      timeRemain.innerHTML = `Time Remaining: 0${quotient}:0${remaining--}`;
+      if (remaining === 0) {
+        quotient--;
+        remaining = 59;
+      }
+    }, 1000);
   }
+
   inputTime.value = "";
 }
 
-/*let countDoawnFunction = setInterval(setAlarm()=>{
-}, 1000);*/
+function timeRemainingTitle(remaining, quotient) {
+  if (remaining < 10 && quotient < 10)
+    return (timeRemain.innerHTML = `Time Remaining: 0${quotient}:0${remaining--}`);
+  else if (remaining < 10 && quotient >= 10)
+    return (timeRemain.innerHTML = `Time Remaining: ${quotient}:0${remaining--}`);
+  else if (remaining > 10 && quotient < 10)
+    return (timeRemain.innerHTML = `Time Remaining: 0${quotient}:${remaining--}`);
+  else
+    return (timeRemain.innerHTML = `Time Remaining: ${quotient}:${remaining--}`);
+}
+
+function playAudio() {
+  audio.play();
+}
+function stopAudio() {
+  audio.pause();
+}
 
 // DO NOT EDIT BELOW HERE
 
