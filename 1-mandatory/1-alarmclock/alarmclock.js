792cd36d18cb9a5329434cc242557bdafc81e7ad
OleksChep <aachep@gmail.com>
2023-01-27 11:13:15 +0000

    alarclock first commit

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+let remainingTime;
+let intervalId;
+
+function setAlarm() {
+  let setTimeValue = document.getElementById("alarmSet").value;
+  remainingTime = setTimeValue;
+  let minutes = Math.floor(remainingTime / 60);
+  let seconds = remainingTime % 60;
+
+  document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${
+    minutes < 10 ? "0" : ""
+  }:${seconds < 10 ? "0" : ""}${seconds}`;
+
+  intervalId = setInterval(() => {
+    if (remainingTime > 0) {
+      remainingTime--;
+      let minutes = Math.floor(remainingTime / 60);
+      let seconds = remainingTime % 60;
+      document.getElementById("timeRemaining").innerText = `Time Remaining: ${
+        minutes < 10 ? "0" : ""
+      } : ${seconds < 10 ? "0" : ""}${seconds}`;
+    } else {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
