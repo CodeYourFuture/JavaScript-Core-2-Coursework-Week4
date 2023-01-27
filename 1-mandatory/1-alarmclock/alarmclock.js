3244bcbe0eb03265d24b338a867e181b721a104c
SanaAsaf <sanaasaf@gmail.com>
2023-01-27 22:23:55 +0000

    Sana Asaf-London 9 - Javascript-Core-2-Coursework-Week4
    Only one mandatory partially done.

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+function setAlarm() {
+  let inputBox = document.querySelector("#alarmSet");
+  let counter = document.querySelector("#timeRemaining");
+  let inputValue = inputBox.value;
+  if (inputValue === ""|| inputValue <=0) {counter.innerHTML="Please enter a valid value"; return ; }
+  else {
+  counter.innerHTML=`Time Remaining:${inputValue}`;
+  let timeLeft = inputValue ; 
+  const Interval = setInterval(()=>{
+    timeLeft--;
+       document.querySelector("#timeRemaining").innerHTML=`Time Remaining: ${timeLeft}`;
+      if(timeLeft === 0){
+        playAlarm();
+        clearInterval(Interval)
+      }
+    },1000);
+  }
+  }
+
+
+let myButton = document.querySelector("#set");
+myButton.addEventListener("click", setAlarm());
 
 // DO NOT EDIT BELOW HERE
 
