1b13cc25d9cbea24e98acfcf35e8ac642057be03
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-01-27 17:49:10 +0000

    AlarmClock - done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeRemaining = document.getElementById("timeRemaining");
+  let number = Number(document.getElementById("alarmSet").value);
+  document.body.style.backgroundColor = "white";
+
+  var countDown = setInterval(function () {
+    let seconds = number % 60;
+    let minutes = Math.floor(number / 60);
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    //changing colors in the end of timer
+    if (number === 3) document.body.style.backgroundColor = "yellow";
+    if (number === 2) document.body.style.backgroundColor = "orange";
+    if (number === 1) document.body.style.backgroundColor = "purple";
+
+    //playing sound and stopping countdown
+    if (number === 0) {
+      document.body.style.backgroundColor = "green";
+      playAlarm();
+      clearInterval(countDown);
+    }
+
+    //pausing alarm
+    document.getElementById("pause").addEventListener("click", () => {
+      clearInterval(countDown);
+    });
+    //changing content
+    timeRemaining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+    number--;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -10,7 +42,7 @@ function setup() {
   });
 
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
+    stopAlarm();
   });
 }
 
@@ -18,7 +50,7 @@ function playAlarm() {
   audio.play();
 }
 
-function pauseAlarm() {
+function stopAlarm() {
   audio.pause();
 }
 
