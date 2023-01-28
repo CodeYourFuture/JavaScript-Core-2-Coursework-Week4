7ab40c8be8fbc167cb168dbd45bcd6c131b4d2a9
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-28 14:22:03 +0000

    Update alarmclock.js

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,8 +1,30 @@
-function setAlarm() {}
+let timeRemaining = 0;
+
+function renderTimeRemaining() {
+  let minutes = Math.floor(timRemaining / 60)
+  let seconds = timeRemaining % 60;
+  const timeRemainingElement = document.querySelector("#timeRemaining");
+  timeRemainingElement.innerText = `Time remaining: ${minutes}:${seconds}`
+}
+
+
+function setAlarm() {
+  const newTime = document.querySelector("#alarmSet").value;
+  timeRemaining = newTime;
+  renderTimeRemaining();
+  setInterval(() => {
+    timeRemaining--;
+    renderTimeRemaining();
+    if (timeRemaining <= 0) {
+      clearInterval(interval);
+      playAlarm()
+    }
+  }, 1000)
+}
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+var audio = new Audio("/alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
