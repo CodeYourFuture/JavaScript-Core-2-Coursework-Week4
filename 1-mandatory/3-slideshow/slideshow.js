17052e12c4a4c3d88d3bdef15f72d3a5d116d2f4
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-29 13:32:07 +0000

    3-slideshow fully functional. Added event handlers and intervals

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -3,6 +3,9 @@ const title = document.getElementById("title");
 const image = document.getElementById("image");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
+const autoback = document.getElementById("autoback");
+const autoforward = document.getElementById("autoforward");
+const stop = document.getElementById("stop");
 const imageArray = [
   "https://images.unsplash.com/photo-1674350369100-bc50d4da3580?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
   "https://images.unsplash.com/photo-1674216709275-61a436e1ecd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
@@ -10,16 +13,18 @@ const imageArray = [
   "https://images.unsplash.com/photo-1674228640216-156de827e0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
 ];
 let imageCounter = 0;
+let intervalId;
+
 title.innerText = "Image Carousel";
 image.src = imageArray[3];
 
 prev.addEventListener("click", () => {
-
-    image.src = imageArray[imageCounter];
-    if (imageCounter == 0) {
-        imageCounter = 3;
-    } else { imageCounter--; }
-
+  image.src = imageArray[imageCounter];
+  if (imageCounter == 0) {
+    imageCounter = 3;
+  } else {
+    imageCounter--;
+  }
 });
 
 next.addEventListener("click", () => {
@@ -30,3 +35,35 @@ next.addEventListener("click", () => {
     imageCounter++;
   }
 });
+
+autoback.addEventListener("click", () => {
+ intervalId = setInterval(autobackFunction, 1000);
+});
+
+autoforward.addEventListener("click", () => {
+  intervalId = setInterval(autoForwardFunction, 1000);
+});
+
+stop.addEventListener("click", () => {
+  clearInterval(intervalId);
+});
+
+
+function autobackFunction() {
+  image.src = imageArray[imageCounter];
+  if (imageCounter == 0) {
+    imageCounter = 3;
+  } else {
+    imageCounter--;
+  }
+}
+
+
+function autoForwardFunction() {
+   image.src = imageArray[imageCounter];
+   if (imageCounter == 3) {
+     imageCounter = 0;
+   } else {
+     imageCounter++;
+   }
+}
