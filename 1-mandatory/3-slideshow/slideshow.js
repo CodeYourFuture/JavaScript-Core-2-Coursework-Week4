e5deb327114f7b61d8f5eea3019059d18ce6a0dd
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-30 01:17:00 +0000

    Extra - 2-slideshow working

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -6,11 +6,13 @@ const next = document.getElementById("next");
 const autoback = document.getElementById("autoback");
 const autoforward = document.getElementById("autoforward");
 const stop = document.getElementById("stop");
+const time = document.getElementById("number");
+time.value = 1000;
 const imageArray = [
-  "https://images.unsplash.com/photo-1674350369100-bc50d4da3580?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  "https://images.unsplash.com/photo-1673452175719-44871107a22f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   "https://images.unsplash.com/photo-1674216709275-61a436e1ecd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
-  "https://images.unsplash.com/photo-1674228640170-e9321a2b2424?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   "https://images.unsplash.com/photo-1674228640216-156de827e0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1673495249470-5d19d64cb127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2NnxDRHd1d1hKQWJFd3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
 ];
 let imageCounter = 0;
 let intervalId;
@@ -37,11 +39,11 @@ next.addEventListener("click", () => {
 });
 
 autoback.addEventListener("click", () => {
- intervalId = setInterval(autobackFunction, 1000);
+ intervalId = setInterval(autobackFunction, Math.floor(time.value));
 });
 
 autoforward.addEventListener("click", () => {
-  intervalId = setInterval(autoForwardFunction, 1000);
+  intervalId = setInterval(autoForwardFunction, Math.floor(time.value)) ;
 });
 
 stop.addEventListener("click", () => {
