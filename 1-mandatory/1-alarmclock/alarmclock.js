271db0dd266b3bed5dacc50488d107dd03f7d0c3
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2023-02-04 02:38:09 +0000

    alarm clock mvp

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,6 +1,63 @@
-function setAlarm() {}
+let bColchange;
+let runClock;
 
-// DO NOT EDIT BELOW HERE
+function setAlarm() {
+let countDownValue = document.getElementById("alarmSet").value; // returns a number 
+  console.log(countDownValue);
+  
+const formatTimeToString = function (numb){
+    let sixty = 60;
+    let mins = Math.floor(numb / sixty);}
+  let secs = numb % sixty;
+  let timeVal = (divTimeVal)=>{
+    return divTimeVal < 10? `0${divTimeVal}`: divTimeVal;    
+  }
+  document.getElementById("timeRemaining").textContent = `Time Remaining: ${timeVal(mins)}:${timeVal(secs)}`;
+}
+  
+const runner = function(){
+  if (countDownValue <= 0){
+    //clear the setInternval or setTimeout function
+    clearInterval(runClock);
+    playAlarm();
+     bColchange = setInterval(changeBackgroundColor, 1000);
+  
+    }
+    //returnCountToDom.textContent = formatTimeToString(countDownValue);
+    formatTimeToString(countDownValue);
+    countDownValue--;
+  };
+  
+  document.getElementById("alarmSet").value = null;
+  let runClock = setInterval(runner, 1000);
+  
+ // -- color ---//
+  // random number function 
+  const randomNumber = (num) => {
+    return Math.floor(Math.random() * num);
+  };
+  //create hsl color
+  const randomColor = () => {
+    const h = randomNumber(360);
+    const s = randomNumber(100);
+    const l = randomNumber(100);
+    return `hsl(${h}deg, ${s}%, ${l}%)`;
+  };
+  // set background color 
+  const changeBackgroundColor = () => {
+    const newRandomColor = randomColor();
+    document.getElementById("background").style.backgroundColor = newRandomColor;
+  };
+
+};
+
+function stopAlarm() {
+    clearInterval(bColchange);
+    console.log("tried to pause alarm");
+  clearInterval(runClock);
+    document.body.style.backgroundColor = 'white'
+}
+  // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
