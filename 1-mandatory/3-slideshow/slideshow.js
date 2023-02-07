bcc6fe9bcc39657cf93038c219c9c6245075816f
Gayle Ngozi <gaylengozi@gmail.com>
2023-02-07 22:35:00 +0000

    slideshow complete

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,25 @@
 // Write your code here
+const images = [
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+];
+const pictures = document.querySelector("#carousel-img");
+
+pictures.src = images[0];
+const forwardButton = document.querySelector("#forward-btn");
+forwardButton.addEventListener("click", () => {
+  images.push(images[0]);
+  console.log(images);
+  images.shift();
+  console.log(images);
+  pictures.src = images[0];
+});
+
+const backwardButton = document.querySelector("#backward-btn");
+// backwardButton.addEventListener("click", ()=> {
+backwardButton.addEventListener("click", () => {
+  images.unshift(images.slice(-1)[0]);
+  console.log(images);
+  pictures.src = images.pop();
+});
