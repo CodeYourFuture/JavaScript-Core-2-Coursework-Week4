bbe25ec7125efa4031780c1e4726b27df17fed9d
BoshraM <gbboshra@gmail.com>
2023-02-09 17:19:54 +0000

    alaramclock done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeRemaining=document.getElementById("timeRemaining");
+  let input=document.getElementById("alarmSet");
+   inputTime=input.value
+  
+    myInterval = setInterval(myTimer, 1000);
+   
+  function myTimer() {
+    inputTime= inputTime-1
+     //divided time to second s=adn minute
+    let minutes = (inputTime / 60).toFixed(0).padStart(2, "0");
+    let seconds = (inputTime % 60).toString().padStart(2, "0");
+    timeRemaining.innerText=`Time Remaining: ${minutes}:${seconds}`;
+    if (inputTime === 0) {
+      playAlarm();
+      clearInterval(myInterval);
+
+    } 
+    }
+ ;
+ 
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -11,7 +32,7 @@ function setup() {
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
-  });
+  }); 
 }
 
 function playAlarm() {
