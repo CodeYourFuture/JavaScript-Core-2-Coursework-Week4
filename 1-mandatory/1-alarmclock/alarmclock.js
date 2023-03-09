d0e02fffff0fd5aba9d13500572343405166ac49
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-03-09 15:08:54 +0000

    alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,24 @@
-function setAlarm() {}
 
+let timeRemaining = 0;
+function renderTimeRemaining(){
+  let minutes = Math.floor(timeRemaining/60);
+  let seconds = timeRemaining % 60;
+  const timeRemainingElement = document.querySelector("#timeRemaining");
+  timeRemainingElement.textContent = (`${minutes}:${seconds}`);
+}
+function setAlarm() {
+const newTime = document.querySelector("#alarmSet").value;
+timeRemaining = newTime;
+renderTimeRemaining();
+const interval = setInterval(() => {
+  timeRemaining--;
+  if(timeRemaining <= 0){
+    clearInterval(interval);
+    playAlarm();
+  }
+  renderTimeRemaining();
+},1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
