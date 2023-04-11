b3d91e8f3136a541dfd15cb7b202e589d8ad0099
fowobi <109865505+fowobi@users.noreply.github.com>
2023-04-11 10:09:22 +0100

    updated

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,71 @@
-function setAlarm() {}
+var countdown;
+var remainingTime;
+
+function setAlarm() {
+  // Get the input field value
+  var input = document.getElementById("alarmSet").value;
+  remainingTime = input;
+
+  document.title = "Time Remaining: 00:" + padZeroes(remainingTime);
+   countdown = setInterval(function() {
+    remainingTime--;
+    document.title = "Time Remaining: 00:" + padZeroes(remainingTime);
+    if (remainingTime == 0) {
+      var flash = setInterval(function() {
+        document.body.style.backgroundColor = document.body.style.backgroundColor == "red" ? "white" : "red";
+      }, 500);
+
+      setTimeout(function() {
+        clearInterval(flash);
+        document.body.style.backgroundColor = "white";
+      }, 5000);
+
+      playAlarm();
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
+
+function padZeroes(number) {
+  if (number < 10) {
+    return "0" + number;
+  } else {
+    return number;
+  }
+}
+
+function pauseAlarm() {
+  clearInterval(countdown);
+}
+
+function resumeAlarm() {
+  countdown = setInterval(function() {
+    remainingTime--;
+    document.title = "Time Remaining: 00:" + padZeroes(remainingTime);
+    if (remainingTime == 0) {
+      var flash = setInterval(function() {
+        document.body.style.backgroundColor = document.body.style.backgroundColor == "red" ? "white" : "red";
+      }, 500);
+
+      setTimeout(function() {
+        clearInterval(flash);
+        document.body.style.backgroundColor = "white";
+      }, 5000);
+      playAlarm();
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
+
+
+
+
+
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
