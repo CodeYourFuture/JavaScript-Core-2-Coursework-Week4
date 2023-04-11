4752dc419cc0e800c9040368f4369f3003b5c383
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-11 01:21:46 +0100

    Mandatory - AlarmClock passes

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,54 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet");//input id
+  let time = input.value;
+  const title = document.getElementById("timeRemaining");//header id
+  title.innerText = `Time Remaining: ${timer(time)}`; //second move for tittle.innerText fixed, passes test 1 and 2
+  console.log(title.innerText);
+
+  const alarmVoice = document.createElement("audio");
+  const voiceSource = document.createElement("source");
+  voiceSource.setAttribute("src", "alarmsound.mp3");
+  alarmVoice.append(voiceSource);
+  title.append(alarmVoice);
+
+  let titleTimer = () => {
+    title.innerText = `Time Remaining: ${timer(time - 1)}`; //tittle.innerText is declared here first
+
+    if (time === 0) {
+      clearInterval(timeLoop);
+      console.log("time is up");
+      playAlarm();
+    }
+    time--;
+  };
+  const timeLoop = setInterval(titleTimer, 1000);
+/*   if (time === 0) {
+    clearInterval(timeLoop);
+    time = 0;
+    title.innerText = `Time Remaining: ${timer(time)}`;
+    console.log("time is up");
+  } */
+}
+
+function addZeroBefore(n) {
+  return (n < 10 ? "0" : "") + n;
+}
+function timer(n) {
+  let res;
+  if (n < 60) {
+    //res = `00:${n}`;
+    res = `00:${addZeroBefore(n)}`;
+  } else if (n > 59) {
+    let min = 0;
+    let sec = 0;
+    min = parseInt(n / 60);
+    sec = n % 60;
+    res = `${addZeroBefore(min)}:${addZeroBefore(sec)}`;
+  }
+  return res;
+}
+//console.log(timer(34))
+//setTimeout(((i1,i2,i3) => {console.log(`${i1} and ${i2} and ${i3}`)}), 3000, "A", "B", "C")
 
 // DO NOT EDIT BELOW HERE
 
