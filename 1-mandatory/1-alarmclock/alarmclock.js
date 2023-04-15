5fea03ad4efdb7396c1fdca177428f09949ad3e0
Shahid Amin <schboostie@gmail.com>
2023-04-15 01:12:00 +0100

    Update alarmclock.js

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,24 @@
-function setAlarm() {}
+function setAlarm() {
+  const heading = document.querySelector("#timeRemaining");
+  const input = document.querySelector("#alarmSet");
+  const button = document.querySelector("#set");
+
+  let timeLeft = parseInt(input.value);
+  let intervalId = setInterval(() => {
+    timeLeft--;
+    const minutes = Math.floor(timeLeft / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timeLeft % 60).toString().padStart(2, "0");
+    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+    if (timeLeft === 0) {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+
+  button.disabled = true;
+}
 
 // DO NOT EDIT BELOW HERE
 
