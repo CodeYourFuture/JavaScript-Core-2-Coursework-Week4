c18ce93fa34c28d2ca903e9ddb65307659045e2f
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-04-15 07:31:48 +0100

    Update alarmclock.js

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  document.getElementById("set").addEventListener("click", function (event) {
+    let inputValue = document.getElementById("alarmSet").value;
+
+    const timeElement = document.querySelector("#timeRemaining");
+    timeElement.innerText = `Time Remaining 00:${inputValue}`;
+
+    function updateTimeRemaining() {
+      inputValue--;
+    }
+
+    function countDownTimer() {
+      if (inputValue >= 0) {
+        timeElement.innerText = `Time Remaining 00:${inputValue}`;
+        updateTimeRemaining();
+      } else playAlarm();
+    }
+    setInterval(countDownTimer, 1000);
+  });
+}
+
+document.getElementById("stop").addEventListener("click", function () {
+  clearTimeout();
+});
 
 // DO NOT EDIT BELOW HERE
 
