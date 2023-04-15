c09903f0a3797f77a8b739709d70e667b7a07fb9
Joseph Mwanza <jmwanzaj@yahoo.com>
2023-04-15 13:32:23 +0100

    Set Alaram is updating the H1

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,15 @@
-function setAlarm() {}
+// document.getElementById("#set").onclick = setAlarm;
+function setAlarm() {
+  let timerDisplay = document.querySelector("h1");
+  let inputNumber = document.querySelector("input").value;
 
+  if (inputNumber < 10) {
+    timerDisplay.innerText = `Time Remaining: 00:0${inputNumber}`;
+  } else {
+    timerDisplay.innerText = `Time Remaining: 00:${inputNumber}`;
+  }
+}
+// setAlarm();
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
