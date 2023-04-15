c80ad56a23b63e5a9bea62ecdb7f467ca827229d
akramizedi <afsoonewss@gmail.com>
2023-04-15 15:46:28 +0100

    challenge 1

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,40 @@
-function setAlarm() {}
+
+setInterval(countDown,1000);
+const duration = 1;
+function countDown(){
+  duration--;
+  console.log(countDown);
+  if (duration === 0) {
+    clearInterval(countDown) 
+
+  }
+}
+
+function playAlarm(){
+audio.play();
+}
+
+
+
+function setAlarm() 
+
+
+// {
+//   let countDown = setInterval(() => {
+//     duration--;
+//     console.log(duration);
+//     if (duration === 0) {
+//       clearInterval(countDown);
+//     }
+//   }, 1000);
+// }
+
+
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,41 @@
 // Write your code here
+const backBtn = document.querySelector("#backbutton");
+const forwardBtn = document.querySelector("#forwardbutton");
+
+const imgArr = [
+  {
+    id:0,
+    src:"ilse-orsel-vmFEBIEz0hQ-unsplash.jpg",
+    alt:"cute cat zero"
+  }
+  {
+    id:1,
+    src: "./assets/cute-cat-a.png",
+    alt: "cute cat first",
+  },
+  {
+    id:2,
+    src: "./assets/cute-cat-b.png",
+    alt: "cute cat second",
+  },
+  {
+    id:3,
+    src: "./assets/cute-cat-c.png",
+    alt: "cute cat third",
+  },
+];
+
+document.addEventListener("DOMContentLoaded" , getImageOnload);
+
+const imgPlace = document.querySelector("#image");
+
+function getImageOnload(){
+    imgPlace.src = imgArr[0].src;
+    imgPlace.alt = imgArr[0].alt;
+    imgPlace.id = imgArr[0].id;
+    document.querySelectorAll("#imgTitle").innerText =0;
+}
+
+backBtn.addEventListener("click",function(){
+  
+})
