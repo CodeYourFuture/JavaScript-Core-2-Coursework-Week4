ec5bf03e17b9ff6507d77f083748f4c45b792039
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-04-15 15:38:56 +0100

    start of the first task

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,36 @@
 // Write your code here
+const backBtn = document.querySelector("#backbutton");
+const forwardBtn = document.querySelector("#forwardBtn");
+
+const imgArr = [
+  {
+    src: "./assets/cute-cat-a.png",
+    alt: "cute cat first pic",
+  },
+  {
+    src: "./assets/cute-cat-b.png",
+    alt: "cute cat second pic",
+  },
+  {
+    src: "./assets/cute-cat-c.png",
+    alt: "cute cat third pic",
+  },
+  {
+    src: "./assets/cute-cat-d.png",
+    alt: "cute cat fourth pic",
+  },
+];
+
+document.addEventListener("DOMContentLoaded", getImageOnload);
+
+const imgPlace = document.querySelector("#image");
+
+function getImageOnload() {
+  imgPlace.src = imgArr[0].src;
+  imgPlace.alt = imgArr[0].alt;
+  document.querySelector("#imgTitle").innerText = 0;
+}
+
+backBtn.addEventListener("")
+
+
