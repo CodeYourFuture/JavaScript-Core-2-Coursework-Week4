3cd4b5da70e1c04c71895666499eb6dccc3c46cb
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-04-16 15:45:51 +0100

    alarm clock done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+function setAlarm() {
+let inputSet = document.querySelector("#alarmSet");
+
+// let minutes = Math.floor( inputSet.value / 60);
+// let seconds = inputSet.value % 60;
+
+// let timeRemaining = document.querySelector("#timeRemaining");
+// timeRemaining.textContent = `Time Remaining: ${minutes}:${seconds}`;
+
+displayRemaining(inputSet.value)
+let totalSeconds = inputSet.value;
+
+let interval = setInterval(() => {
+  totalSeconds -= 1;
+  displayRemaining(totalSeconds)
+
+if (totalSeconds === 0) {
+  playAlarm()
+  clearInterval(interval)
+}
+}, 1000)
+
+}
+
+
+
+function displayRemaining(totalSeconds){
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+
+let timeRemaining = document.querySelector("#timeRemaining");
+timeRemaining.textContent = `Time Remaining:     ${minutes}:${seconds}`;
+} 
+
 
 // DO NOT EDIT BELOW HERE
 
