ea4755f6f056e8eb38d2db5a688bed38559e94d3
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-04-16 17:25:55 +0100

    done

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,6 +1,8 @@
 // Write your code here
-const backBtn = document.querySelector("#backbutton");
-const forwardBtn = document.querySelector("#forwardBtn");
+const backBtn = document.querySelector("#backward-btn");
+const forwardBtn = document.querySelector("#forward-btn");
+const autoFrwBtn = document.querySelector("#auto-forward");
+const autoBwBtn = document.querySelector("#auto-backward");
 
 const imgArr = [
   {
@@ -8,22 +10,22 @@ const imgArr = [
     alt: "cute cat first pic",
   },
   {
-    src: "./assets/cute-cat-b.png",
+    src: "./assets/cute-cat-b.jpg",
     alt: "cute cat second pic",
   },
   {
-    src: "./assets/cute-cat-c.png",
+    src: "./assets/cute-cat-c.jpg",
     alt: "cute cat third pic",
   },
-  {
-    src: "./assets/cute-cat-d.png",
-    alt: "cute cat fourth pic",
-  },
+  // {
+  //   src: "./assets/cute-cat-d.jpg",
+  //   alt: "cute cat fourth pic",
+  // },
 ];
 
 document.addEventListener("DOMContentLoaded", getImageOnload);
 
-const imgPlace = document.querySelector("#image");
+const imgPlace = document.querySelector("#carousel-img");
 
 function getImageOnload() {
   imgPlace.src = imgArr[0].src;
@@ -31,6 +33,57 @@ function getImageOnload() {
   document.querySelector("#imgTitle").innerText = 0;
 }
 
-backBtn.addEventListener("")
+let moveBack = function () {
+    let index = Number(document.querySelector("#imgTitle").innerText);
+    
+    if (index === 0) {
+        index = imgArr.length -1;
+    } else {
+        index--;
+    }
+    
+    imgPlace.src = imgArr[index].src;
+    imgPlace.alt = imgArr[index].alt;
+    document.querySelector("#imgTitle").innerText = index;
+
+}
+
+backBtn.addEventListener("click", moveBack);
+
+let moveForward = function () {
+  let index = Number(document.querySelector("#imgTitle").innerText);
+
+  if (index === imgArr.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  imgPlace.src = imgArr[index].src;
+  imgPlace.alt = imgArr[index].alt;
+  document.querySelector("#imgTitle").innerText = index;
+};
+
+forwardBtn.addEventListener("click", moveForward);
+
+let backInterval;
+let forwardInterval; 
 
 
+autoBwBtn.addEventListener("click", function () {
+  autoBwBtn.disabled = true;
+  autoFrwBtn.disabled = true;
+  backInterval = setInterval(moveBack, 2000) });
+
+
+autoFrwBtn.addEventListener("click", function () {
+  autoBwBtn.disabled = true;
+  autoFrwBtn.disabled = true;
+  forwardInterval = setInterval(moveForward, 2000)});
+
+document.querySelector("#stop").addEventListener("click", function () {
+  autoBwBtn.disabled = false;
+  autoFrwBtn.disabled = false;
+  clearInterval(backInterval);
+  clearInterval(forwardInterval);
+})
