60db7bfb884dc524c79eb5b6581d0ebd5d694ad0
Olha Danylevska <odanilevskaya2@gmail.com>
2023-04-18 18:12:20 +0100

    Update alarmclock.js

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -6,40 +6,32 @@ function setAlarm() {
   let timeLine = document.querySelector("#timeRemaining")
   let givenTime = inputTime.value 
   
-
-      var equalMinutes = Math.floor(givenTime/60);
-      var rest = givenTime % 60;
-      console.log(equalMinutes)
-      timeLine.innerText = `Time Remaining: ${equalMinutes.toString().padStart(2, "0")}:${rest.toString().padStart(2, "0")}`
+      let minutes = Math.floor(givenTime/60);
+      let seconds = givenTime % 60;
+      console.log(minutes)
+      timeLine.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
    
       givenTime = givenTime - 1
   
   let myInterval = setInterval(function(){ 
-      if (givenTime == 0) {
+      if (givenTime < 0) {
         clearInterval(myInterval);
-        timeLine.innerText = `Time Remaining: 00:00`
         audio.play()
-      }else if (givenTime < 10 && givenTime >= 0){
-        timeLine.innerText = `Time Remaining: 00:0${givenTime}`
-      } else if(givenTime >=60){
-        equalMinutes = Math.floor(givenTime/60);
-        rest = givenTime % 60;
-        timeLine.innerText = `Time Remaining: ${equalMinutes.toString().padStart(2, "0")}:${rest.toString().padStart(2, "0")}`
-      } else if (givenTime >= 10 && givenTime < 60) {
-        timeLine.innerText = `Time Remaining: 00:${givenTime}`
+      } else{
+        minutes = Math.floor(givenTime/60);
+        seconds = givenTime % 60;
+        timeLine.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
       } 
       givenTime --    
   }, 1000);
 }
 
 function stopAlarm(){
-  let stopButton = document.querySelector("#stop")
-  stopButton.addEventListener("click", function(event) {
-    event.preventDefault()
     audio.stop()
-  });
-
 }
+
+let stopButton = document.querySelector("#stop")
+stopButton.addEventListener("click", stopAlarm)
 
 setAlarmButton.addEventListener("click", setAlarm)
 
