e65e3daf9b7a1e7dafd40b1aa0115b21322c7c6b
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-04-19 22:51:41 +0100

    1-alarmclock task done
    task done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,21 @@
-function setAlarm() {}
+function setAlarm() {
+  const timeInput = document.getElementById("alarmSet");
+  const timeRemainingDisplay = document.getElementById("timeRemaining");
+  let timeToSet = timeInput.value;
+  function countDown (){
+    timeToSet--; //means -1
+  let minutes = Math.floor(timeToSet / 60)
+  let seconds = timeToSet % 60;
+  let convertedTime = `${minutes}:${seconds}`;
+  timeRemainingDisplay.innerText = `Time remaining: ${convertedTime}`;
+   if(timeToSet === 0){
+        clearInterval(Interval) 
+        playAlarm()}
+
+  }
+  let Interval = setInterval(countDown, 1000);
+  
+}
 
 // DO NOT EDIT BELOW HERE
 
