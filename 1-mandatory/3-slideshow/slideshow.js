2a3e0154defdc5befa15f3c81c470fecd9fe4b64
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2023-04-20 07:38:44 +0100

    finished the next and previous buttons

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,38 @@
-// Write your code here
+const photos = [
+  "https://images.unsplash.com/photo-1523910062200-d2ee1971de7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+
+  "https://images.unsplash.com/photo-1583185732932-e42a0858df74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+
+  "https://images.unsplash.com/photo-1573523120347-6ec1a1959c3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80",
+
+  "https://images.unsplash.com/photo-1576224975829-675f460f9288?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+];
+
+
+
+let slide = document.querySelector("#slide")
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+
+slide.style.width = "700px"
+slide.style.height = "700px"
+
+let i = 0
+slide.src = photos[i]
+
+btn2.onclick = () => {
+    i++
+    if (i > photos.length -1) {
+        i = 0
+    }
+    slide.src = photos[i]
+}
+
+btn1.onclick = () => {
+    i--
+    if (i < 0) {
+        i = photos.length -1
+    }
+    slide.src = photos[i]
+}
+
