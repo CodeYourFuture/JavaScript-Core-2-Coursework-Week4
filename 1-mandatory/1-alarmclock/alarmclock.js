b11b4a31c58c0f412b63822cf75aa903751259f6
Man Sang Sin <man.sang.sin@gmail.com>
2023-04-21 16:54:01 +0100

    created functions
    - created a function to convert time from seconds to minutes and seconds
    - created a function to display the time string on the html page

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,56 @@
-function setAlarm() {}
+function getTimeString(timeInSeconds) {
+  let timeSeconds = 0;
+  let timeMinutes = 0;
+  if (timeInSeconds / 60 >= 1) {
+    timeSeconds = timeInSeconds % 60;
+    timeMinutes = Math.floor(timeInSeconds / 60);
+  } else {
+    timeSeconds = timeInSeconds;
+  }
+  if (timeMinutes < 10) {
+    timeMinutes = `0${timeMinutes}`;
+  }
+  if (timeSeconds < 10) {
+    timeSeconds = `0${timeSeconds}`;
+  }
+  return `Time Remaining: ${timeMinutes}:${timeSeconds}`;
+}
+
+function renderTime(convertedTimeString) {
+  let displayTime = document.querySelector("#timeRemaining");
+  displayTime.innerText = convertedTimeString;
+  // console.log(convertedTimeString);
+}
+
+function setAlarm() {
+  var inputTime = document.querySelector("#alarmSet").value;
+  renderTime(getTimeString(inputTime));
+  test(inputTime);
+}
+let counter = 0;
+function decreaseTimeOne(timeIn) {
+  console.log(`timeout1: ${timeOut}`);
+  var timeOut = timeIn - counter;
+  console.log(`timeout2: ${timeOut}`);
+  counter++;
+  return timeOut;
+}
+
+function test(variable1) {
+  const changeTimeID = setInterval(function () {
+    timeNow = decreaseTimeOne(variable1);
+    console.log(`test${timeNow}`);
+    renderTime(getTimeString(timeNow));
+  }, 1000);
+}
+
+let setBtn = document.querySelector("#set");
+setBtn.addEventListener("click", setAlarm);
+
+// function test() {
+//   inputText = document.querySelector("#alarmSet");
+//   alert(inputText.value);
+// }
 
 // DO NOT EDIT BELOW HERE
 
