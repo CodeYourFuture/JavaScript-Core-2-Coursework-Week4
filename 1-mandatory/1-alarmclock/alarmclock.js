7909f2d8c0b68bcac8bb7f9dab7c7859f3833829
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-04-25 21:01:06 +0100

    1-alarmclock
    Everything is working fine, but the test fails.

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,44 @@
-function setAlarm() {}
+function setAlarm() {
+  let alarmSet = document.querySelector("#alarmSet").value;
+  let set = document.querySelector("#set");
+  let stop = document.querySelector("#stop");
+  let timeRemaining = document.querySelector("#timeRemaining");
+
+  timeRemaining.innerHTML = `Time Remaining: ${alarmSet}:00`;
+  let count = setInterval(() => {
+    let timeLeft = timeRemaining.innerHTML.split(" ")[2].split(":");
+    let minutes = parseInt(timeLeft[0]);
+    let seconds = parseInt(timeLeft[1]);
+
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(count);
+      playAlarm();
+      document.body.style.backgroundColor = "red";
+    } else if (seconds === 0) {
+      minutes -= 1;
+      seconds = 59;
+    } else {
+      seconds -= 1;
+    }
+
+    timeRemaining.innerHTML =
+      "Time Remaining: " +
+      (minutes < 1 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 1 ? "0" : "") +
+      seconds;
+  }, 1000);
+
+  stop.addEventListener("click", function () {
+    clearInterval(count);
+    alarmSet.valid = "";
+
+    pauseAlarm();
+    timeRemaining.innerHTML = "Time Remaining: 00:00";
+    document.body.style.backgroundColor = "white";
+  });
+}
 
 // DO NOT EDIT BELOW HERE
 
