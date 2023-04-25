9d600f250fa3194f72c569959cc316abcfb39649
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-04-25 22:20:26 +0100

    JS done

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,7 +1,34 @@
+function dispalyRemainingTime(totalSeconds) {
+  let timeRemaining = document.getElementById("timeRemaining");
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+
+  // timeRemaining.textContent = "Time Remaining  " + minutes + ":" + seconds;
+
+  if (minutes < 10) {
+    timeRemaining.textContent =
+      "Time Remaining  " + "0" + minutes + ":" + seconds;
+  } else if (seconds < 10) {
+    timeRemaining.textContent =
+      "Time Remaining  " + minutes + ":" + "0" + seconds;
+  } else {
+    timeRemaining.textContent = "Time Remaining  " + minutes + ":" + seconds;
+  }
+}
+
 function setAlarm() {
-  //well get what the user inputs.
-  var userInput = document.getElementById("timeRemaining").value;
-  
+  let inputTime = document.getElementById("alarmSet");
+  let totalSeconds = inputTime.value;
+  dispalyRemainingTime(totalSeconds);
+
+  let Interval = setInterval(() => {
+    totalSeconds -= 1;
+    dispalyRemainingTime(totalSeconds);
+    if (totalSeconds === 0) {
+      playAlarm();
+      clearInterval(Interval);
+    }
+  }, 1000);
 }
 
 // DO NOT EDIT BELOW HERE
