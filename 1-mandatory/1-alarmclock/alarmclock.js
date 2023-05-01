8ac631568560ecfd11351982a9a867deadfaf210
PaulinaWywrot <wywrot.paula@gmail.com>
2023-05-01 19:23:54 +0100

    mandatory

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  const timeRemaining = document.querySelector("#timeRemaining");
+  const input = document.querySelector("#alarmSet");
+  let inputValue = Number(input.value);
+  document.getElementById("set").addEventListener("click", () => {
+    clearInterval(countdown);
+  });
+  let countdown = setInterval(function () {
+    let seconds = inputValue % 60;
+    let minutes = Math.floor(inputValue / 60);
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (inputValue === 0) {
+      playAlarm();
+      clearInterval(countdown);
+    }
+    timeRemaining.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+    inputValue--;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
