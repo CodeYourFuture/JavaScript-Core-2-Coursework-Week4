55572749e42261ccd493c3d2bd921831b35f39f1
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-05-03 23:53:39 +0100

    error in alarm clock

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  //here to store the input from user in a variable
+  let inputEl = document.getElementById("alarmSet");
+  let timeFromUser = inputEl.value;
+  let setAlarmButton = document.getElementById("set");
+
+  let h1El = document.getElementById("timeRemaining");
+  // h1El.innerHTML = `Time Remaining: ${timeFromUser}`;
+  //how to format the time entered from user to look like 00:00
+  // then how to start decreasing one by one
+  //maybe for loops lets try
+  for (let seconds = timeFromUser; seconds >= 0; seconds--) {
+    console.log(`Time Remaining: ${seconds}`);
+    h1El.innerHTML = `Time Remaining: ${seconds}`;
+    console.log(h1El.innerHTML);
+  }
+  setInterval(function () {
+    for (let seconds = timeFromUser; seconds >= 0; seconds--) {
+      console.log(`Time Remaining: ${seconds}`);
+      h1El.innerHTML = `Time Remaining: ${seconds}`;
+      console.log(h1El.innerHTML);
+    }
+  }, 10);
+}
 
 // DO NOT EDIT BELOW HERE
 
