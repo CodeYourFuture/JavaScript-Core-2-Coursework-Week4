cb9429fe439532ae862513802a20ecdc169be10e
Afsha10 <115444059+Afsha10@users.noreply.github.com>
2023-05-08 01:32:00 +0100

    Changed the alarmclock.js let var to const var.

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -2,38 +2,37 @@ function displayRemainingTime(totalSeconds) {
   // 1. we get the html element for the time remaining part
   // 2. we want to figure out what this new string would be eg calucuate if it is 100
   // 3. use the value in step 2 and put it in the html element in step 1
-  
-    let timeRemainingString = document.getElementById("timeRemaining");
-    let minutes = Math.floor(totalSeconds / 60); // "Time Remaining: 00:00";
-    let seconds = totalSeconds % 60;
 
-    let formattedMinutes = ("0" + minutes).slice(-2);
-    let formattedSeconds = ("0" + seconds).slice(-2);
+  const timeRemainingString = document.getElementById("timeRemaining");
+  const minutes = Math.floor(totalSeconds / 60); // "Time Remaining: 00:00";
+  const seconds = totalSeconds % 60;
 
-    timeRemainingString.textContent =
-      "Time Remaining: " + formattedMinutes + ":" + formattedSeconds;
-} 
+  const formattedMinutes = ("0" + minutes).slice(-2);
+  const formattedSeconds = ("0" + seconds).slice(-2);
+
+  timeRemainingString.textContent =
+    "Time Remaining: " + formattedMinutes + ":" + formattedSeconds;
+}
 
 function setAlarm() {
-  let inputElement = document.getElementById("alarmSet");
+  const inputElement = document.getElementById("alarmSet");
   // console.log(inputElement.value);
   displayRemainingTime(inputElement.value);
   // 1. we need to store the value of the seconds input
   // 2. every time a second ticks, we want the seconds timer (value of the variable in step 1) to reduce by 1 second
   // 3. display the new reduced amount
-  
+
   let totalSeconds = inputElement.value;
-  
-  let interval = setInterval(() => {
+
+  const interval = setInterval(() => {
     totalSeconds -= 1;
     displayRemainingTime(totalSeconds);
     if (totalSeconds === 0) {
       playAlarm();
       clearInterval(interval);
     }
-  }, 1000)
+  }, 1000);
 }
-
 
 // DO NOT EDIT BELOW HERE
 
