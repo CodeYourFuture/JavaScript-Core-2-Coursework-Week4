6ec73ffcc06b2d096f97fdafc8ec8acfffd57da6
nasir ali <a.nasirali@outlook.com>
2023-05-10 11:39:38 +0100

    fixed 1 mandatory 1 alarm

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  const inputValue = document.getElementById("alarmSet");
+  const timeRemaining = document.getElementById("timeRemaining");
+  let minuteCounter = Math.floor(inputValue.value / 60);
+  let secondCounter = inputValue.value - Math.floor(inputValue.value / 60) * 60;
+  counter();
+
+  function counter() {
+    if (minuteCounter == 0 && secondCounter == 0) {
+      timeRemaining.innerText = `Time Remaining: 0${minuteCounter}:0${secondCounter}`;
+      clearInterval(repeatCounter);
+      playAlarm();
+    } else if (minuteCounter == 0 && secondCounter > 0) {
+      const formattedSeconds =
+        secondCounter < 10 ? `0${secondCounter}` : secondCounter;
+      timeRemaining.innerText = `Time Remaining: 0${minuteCounter}:${formattedSeconds}`;
+      secondCounter--;
+    } else if (minuteCounter > 0) {
+      if (secondCounter == 0) {
+        minuteCounter--;
+        secondCounter = 59;
+      }
+      const formattedMinutes =
+        minuteCounter < 10 ? `0${minuteCounter}` : minuteCounter;
+      const formattedSeconds =
+        secondCounter < 10 ? `0${secondCounter}` : secondCounter;
+      timeRemaining.innerText = `Time Remaining: ${formattedMinutes}:${formattedSeconds}`;
+      secondCounter--;
+    }
+  }
+
+  const repeatCounter = setInterval(counter, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -19,6 +51,7 @@ function playAlarm() {
 }
 
 function pauseAlarm() {
+  // clearInterval(repeatCounter); // Clear the interval using the repeatCounter variable
   audio.pause();
 }
 
