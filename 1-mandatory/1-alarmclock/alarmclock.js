05e25c209a91596ade55386a9973c61b9bf76227
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-05-23 22:51:55 +0100

    Alarm Clock Completed

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+function setAlarm() {
+  var setAlarmInput = document.getElementById("alarmSet");
+  var timeRemainingCounter = document.getElementById("timeRemaining");
+
+  var alarmTime = parseInt(setAlarmInput.value);
+  var secondsRemaining = alarmTime;
+
+  var interval = setInterval( function() {
+    secondsRemaining--;
+    
+    var minutes = Math.floor(secondsRemaining / 60);
+    
+    var seconds = secondsRemaining % 60; 
+    
+    var goodFormatTime = ("00" + minutes).slice(-2) + ":" + ("00" + seconds).slice(-2);
+    
+    timeRemainingCounter.textContent = "Time Remaining: " + goodFormatTime;
+
+    if (secondsRemaining === 0) {
+      clearInterval(interval);
+      playAlarm();
+      document.body.style.backgroundColor = "lightblue";
+    }
+
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
