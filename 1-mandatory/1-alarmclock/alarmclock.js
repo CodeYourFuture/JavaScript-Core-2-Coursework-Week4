b51fa3b2a49555cbfbc9471823414f5cfd4d526d
Mr.Demir <mehmetomer806@gmail.com>
2023-05-24 02:48:21 +0100

    last

--- 1-mandatory/1-alarmclock/alarmclock.js
+++ 1-mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,50 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet"); //input id
+  let time = input.value;
+  const title = document.getElementById("timeRemaining"); //header id
+  title.innerText = `Time Remaining: ${timer(time)}`; //second move for tittle.innerText fixed, passes test 1 and 2
+  console.log(title.innerText);
+
+  const alarmVoice = document.createElement("audio");
+  const voiceSource = document.createElement("source");
+  voiceSource.setAttribute("src", "alarmsound.mp3");
+  alarmVoice.append(voiceSource);
+  title.append(alarmVoice);
+
+  let titleTimer = () => {
+    title.innerText = `Time Remaining: ${timer(time - 1)}`; //tittle.innerText is declared here first
+
+    if (time === 0) {
+      clearInterval(timeLoop);
+/*       setInterval(function () {
+        document.body.style.backgroundColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+      }, 100); */
+      console.log("time is up");
+      playAlarm();
+    }
+    time--;
+  };
+  const timeLoop = setInterval(titleTimer, 1000);
+}
+
+function addZeroBefore(n) {
+  return (n < 10 ? "0" : "") + n;
+}
+function timer(n) {
+  let res;
+  if (n < 60) {
+    //res = `00:${n}`;
+    res = `00:${addZeroBefore(n)}`;
+  } else if (n > 59) {
+    let min = 0;
+    let sec = 0;
+    min = parseInt(n / 60);
+    sec = n % 60;
+    res = `${addZeroBefore(min)}:${addZeroBefore(sec)}`;
+  }
+  return res;
+}
 
 // DO NOT EDIT BELOW HERE
 
--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,70 @@
-// Write your code here
+
+const images = [
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+];
+
+let currentIndex = 0;
+
+function changeImages() {
+  const image = document.getElementById("carousel-img");
+  image.src = images[0];
+
+  const forwardBtn = document.getElementById("forward-btn");
+  const backBtn = document.getElementById("backward-btn");
+
+  forwardBtn.addEventListener("click", function () {
+    currentIndex++;
+    if (currentIndex == images.length) {
+      currentIndex = 0;
+    }
+    image.src = images[currentIndex];
+  });
+
+  backBtn.addEventListener("click", function () {
+    currentIndex--;
+    if (currentIndex == -1) {
+      currentIndex = images.length - 1;
+    }
+    image.src = images[currentIndex];
+  });
+
+  //level-2
+  const autoBackBtn = document.getElementById("auto-backward");
+  const autoForwardBtn = document.getElementById("auto-forward");
+  const stopBtn = document.getElementById("stop");
+
+  stopBtn.addEventListener("click", function () {
+    clearInterval(intervalForward);
+    clearInterval(intervalBackward);
+    autoForwardBtn.disabled = false;
+    autoBackBtn.disabled = false;
+  });
+
+  let intervalForward;
+  autoForwardBtn.addEventListener("click", function () {
+    intervalForward = setInterval(function () {
+      currentIndex++
+      currentIndex = (currentIndex) % images.length;
+      image.src = images[currentIndex];
+    }, 2000);
+    autoForwardBtn.disabled = true;
+    autoBackBtn.disabled = true;
+  });
+
+  let intervalBackward;
+  autoBackBtn.addEventListener("click", function () {
+    intervalBackward = setInterval(function () {
+      currentIndex--;
+      if (currentIndex == -1) {
+        currentIndex = images.length - 1;
+      }
+      image.src = images[currentIndex];
+    }, 2000);
+    autoForwardBtn.disabled = true;
+    autoBackBtn.disabled = true;
+  });
+}
+
+changeImages();
