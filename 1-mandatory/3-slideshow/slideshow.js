4dc2bd4eb5b58c3756d4d534f47be90eff665b06
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-05-24 20:22:34 +0100

    1,2 Done 3 Forward button done

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,34 @@
 // Write your code here
+
+
+
+const imageContainer = document.querySelector(".image_container");
+const images = imageContainer.querySelectorAll("img");
+const theForwardBtn = document.querySelector(".forward");
+
+let imageIndex = 0;
+
+showImage(imageIndex);
+
+theForwardBtn.addEventListener("click", showNextImage);
+
+function showImage(index) {
+    images.forEach(image => {
+        image.style.display = "none";
+    });
+
+ // const startIndex = imagesIndex % images.length;
+//  const endIndex = (startIndex + Math.ceil(images.legth / 3)) % images.length; 
+
+//  for (let i = startIndex; i <= endIndex; i++) {
+    images[index].style.display = "block";
+  }  
+
+
+function showNextImage() {
+    imageIndex++;
+    if (imageIndex >= images.length) {
+        imageIndex = 0;
+    }
+    showImage(imageIndex);
+}
