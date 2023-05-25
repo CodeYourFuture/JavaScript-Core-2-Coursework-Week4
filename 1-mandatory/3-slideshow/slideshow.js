7e37654205e4c0f10d2918fd2c5b0acc508e29c3
Damilola Odumosu <dt.odumosu@gmail.com>
2023-05-25 17:23:32 +0100

    uncompleted

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,4 +1,40 @@
-// Write your code here
-let imageSlide = [
-    
-]
+
+let i = 0;  //current image index
+let time = 2000; // timer
+let images = []
+
+images[0] =
+  "https://i.guim.co.uk/img/media/f75f84778dc6c20e519223cdd39ec0f974f2c9b0/0_239_3583_2150/master/3583.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=679a9ea087d210112f70eda780c6fed1";
+images[1] =
+  "https://www.realgap.co.uk/tpl/lib/img/private/media/south-africa-223-copy.jpeg";
+images[2] =
+  "https://www.nhm.ac.uk/content/dam/nhmwww/discover/wpy-56/wpy-first-look-2020-image/hannah-vijayan-wpy56-full-width.jpg.thumb.1160.1160.jpg";
+images[3] =
+  "https://www.worldanimalprotection.org/sites/default/files/styles/600x400/public/media/us_files/1014873_0.jpg?h=de92a0b7&itok=HWjFVXjf";
+
+
+
+
+const imageSlides = document.querySelector("img");
+const btnForForward = document.querySelector("btn-forward");   
+const btnForBackward = document.querySelector("btn-backward")
+const btnForAutoPlay = document.querySelector("btn-play");
+const btnForPause = document.querySelector("btn-pause");
+
+
+btnForForward.addEventListener
+btnForBackward.addEventListener
+btnForAutoPlay.addEventListener
+btnForPause.addEventListener
+
+
+
+
+function slideImage(){
+
+}
+
+function autoSlide(){
+
+}
+setTimeout()
